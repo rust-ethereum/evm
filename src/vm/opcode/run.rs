c6d943f2da52393adb99fc018044fb5bb98cd9ed
Wei Tang <hi@that.world>
2017-04-03 18:01:47 +0800

    Problem: Opcode is C-like Solution: Make PUSH, DUP, SWAP, LOG Rust-like to simplify implementation

--- src/vm/opcode/run.rs
+++ src/vm/opcode/run.rs
@@ -3,13 +3,6 @@ use vm::{Machine, Memory, Stack, PC};
 
 // TODO: deal with gas limit and other Ethereum-specific things.
 
-fn push<S: Stack>(opcode: Opcode, pc: &mut PC, stack: &mut S) {
-    let code_u8: u8 = opcode.into();
-    let count = code_u8 - 0x5f;
-    let val = pc.read(count as usize);
-    stack.push(val);
-}
-
 impl Opcode {
     pub fn run<M: Memory, S: Stack>(&self, machine: &mut Machine<M, S>) {
         let pc = &mut machine.pc;
@@ -52,18 +45,10 @@ impl Opcode {
 
             // TODO: implement omitted opcodes.
 
-            Opcode::PUSH1 | Opcode::PUSH2 | Opcode::PUSH3 |
-            Opcode::PUSH4 | Opcode::PUSH5 | Opcode::PUSH6 |
-            Opcode::PUSH7 | Opcode::PUSH8 | Opcode::PUSH9 |
-            Opcode::PUSH10 | Opcode::PUSH11 | Opcode::PUSH12 |
-            Opcode::PUSH13 | Opcode::PUSH14 | Opcode::PUSH15 |
-            Opcode::PUSH16 | Opcode::PUSH17 | Opcode::PUSH18 |
-            Opcode::PUSH19 | Opcode::PUSH20 | Opcode::PUSH21 |
-            Opcode::PUSH22 | Opcode::PUSH23 | Opcode::PUSH24 |
-            Opcode::PUSH25 | Opcode::PUSH26 | Opcode::PUSH27 |
-            Opcode::PUSH28 | Opcode::PUSH29 | Opcode::PUSH30 |
-            Opcode::PUSH31 | Opcode::PUSH32
-                => push(opcode, pc, stack),
+            Opcode::PUSH(v) => {
+                let val = pc.read(v);
+                stack.push(val);
+            },
 
             _ => {
                 unimplemented!();
