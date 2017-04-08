5172b439e57cb35442ea460fd98091bddb244a13
Wei Tang <hi@that.world>
2017-04-08 20:20:07 +0800

    Problem: SDIV is not implemented Solution: Implement it!

--- src/vm/opcode/run.rs
+++ src/vm/opcode/run.rs
@@ -1,7 +1,18 @@
 use super::Opcode;
+use utils::u256::U256;
 use vm::{Machine, Memory, Stack, PC};
 
 // TODO: deal with gas limit and other Ethereum-specific things.
+
+fn signed_abs(v: U256) -> U256 {
+    let negative: U256 = U256::one() << 256;
+
+    if v >= negative {
+        !v + 1.into()
+    } else {
+        v
+    }
+}
 
 impl Opcode {
     pub fn run<M: Memory, S: Stack>(&self, machine: &mut Machine<M, S>) {
@@ -40,6 +51,37 @@ impl Opcode {
                     stack.push(0.into());
                 } else {
                     stack.push(op1 / op2);
+                }
+            },
+
+            Opcode::SDIV => {
+                // This is signed division. So the U256 would need to
+                // be treated as signed two's complement. We currently
+                // convert it to both positive, and then deal with the
+                // sign afterwards...
+
+                let negative: U256 = U256::one() << 256;
+                // This value is also -2^255 in two's complement.
+                let max: U256 = U256::max_value();
+                // This value is also -1 in two's complement.
+
+                let op1 = stack.pop();
+                let op2 = stack.pop();
+                if op2 == 0.into() {
+                    stack.push(0.into());
+                } else if op1 == negative && op2 == max {
+                    stack.push(negative);
+                } else {
+                    let aop1 = signed_abs(op1);
+                    let aop2 = signed_abs(op2);
+                    let r = op1 / op2;
+
+                    if (op1 < negative && op2 < negative) || (op1 >= negative && op2 >= negative) {
+                        stack.push(r);
+                    } else {
+                        let sr = !r + 1.into();
+                        stack.push(sr);
+                    }
                 }
             },
 
