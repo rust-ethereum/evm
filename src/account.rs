311f3af8cfa6dff294c148d49e8c03233c96234e
Wei Tang <hi@that.world>
2017-04-10 13:58:48 +0800

    Problem: merkle and ring only support Sha2 Solution: use a fork of merkle with crypto

--- src/account.rs
+++ src/account.rs
@@ -1,6 +1,6 @@
 use utils::u256::U256;
-use ring::digest::{Algorithm, Context, SHA256};
 use merkle::{MerkleTree};
+use crypto::sha3::Sha3;
 
 pub struct Account {
     nonce: usize,
@@ -9,14 +9,12 @@ pub struct Account {
     codeHash: U256,
 }
 
-static digest: &'static Algorithm = &SHA256;
-
 impl Default for Account {
     fn default() -> Account {
         Account {
             nonce: 0,
             balance: 0.into(),
-            storageRoot: MerkleTree::from_vec(digest, Vec::new()),
+            storageRoot: MerkleTree::from_vec(Sha3::keccak256(), Vec::new()),
             codeHash: 0.into()
         }
     }
