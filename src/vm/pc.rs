3e696d0f89efd881883aed7507127ea80e608701
Wei Tang <hi@that.world>
2017-04-13 16:42:34 +0800

    Problem: PC does not support other implementations other than slice Solution: Make it a trait

--- src/vm/pc.rs
+++ src/vm/pc.rs
@@ -2,50 +2,73 @@ use utils::u256::U256;
 use std::cmp::{min};
 use super::opcode::Opcode;
 
-pub struct PC<'a> {
-    pub position: usize,
-    code: &'a [u8],
+pub trait PC {
+    fn peek_opcode(&self) -> Opcode;
+    fn read_opcode(&mut self) -> Opcode;
+    fn stop(&mut self);
+    fn stopped(&self) -> bool;
+    fn read(&mut self, byte_count: usize) -> U256;
+    fn position(&self) -> usize;
+}
+
+pub struct VectorPC {
+    position: usize,
+    code: Vec<u8>,
     stopped: bool
 }
 
-impl<'a> PC<'a> {
-    pub fn new(code: &'a [u8]) -> Self {
+impl VectorPC {
+    pub fn new(code: &[u8]) -> Self {
         PC {
             position: 0,
-            code: code,
+            code: code.into(),
             stopped: false,
         }
     }
+}
 
-    pub fn peek_opcode(&self) -> Opcode {
+impl<A: Account> From<&A> for VectorPC {
+    fn from(account: &A) -> VectorPC {
+        let empty: [u8; 0] = [];
+        let code = account.code;
+        if code.is_some() {
+            VectorPC::new(code.unwrap())
+        } else {
+            VectorPC::new(empty)
+        }
+    }
+}
+
+impl PC for VectorPC {
+    fn position(&self) -> usize {
+        self.position
+    }
+
+    fn peek_opcode(&self) -> Opcode {
         let position = self.position;
         let opcode: Opcode = self.code[position].into();
         opcode
     }
 
-    pub fn read_opcode(&mut self) -> Opcode {
+    fn read_opcode(&mut self) -> Opcode {
         let position = self.position;
         let opcode: Opcode = self.code[position].into();
         self.position += 1;
         opcode
     }
 
-    pub fn stop(&mut self) {
+    fn stop(&mut self) {
         self.stopped = true;
     }
 
-    pub fn is_stopped(&self) -> bool {
+    fn stopped(&self) -> bool {
         self.stopped || self.position >= self.code.len()
     }
 
-    pub fn read(&mut self, byte_count: usize) -> U256 {
+    fn read(&mut self, byte_count: usize) -> U256 {
         let position = self.position;
         self.position += byte_count;
         let max = min(position + byte_count, self.code.len());
         U256::from(&self.code[position..max])
-    }
-
-    fn len(&self) -> usize {
-        self.code.len()
     }
 }
