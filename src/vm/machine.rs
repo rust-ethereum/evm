bc1057fad33932a7ba3766d404200697f9eee0e8
Wei Tang <hi@that.world>
2017-04-13 16:44:51 +0800

    Problem: Machine generics makes it hard to append new types to it Solution: (WIP) use associate types

--- src/vm/machine.rs
+++ src/vm/machine.rs
@@ -1,27 +1,95 @@
 use super::{Memory, VectorMemory, Stack, VectorStack, PC, Result, Gas};
-use ::account::Account;
+use account::{Account, FakeAccount};
 
-pub type VectorMachine<'p> = Machine<'p, VectorMemory, VectorStack>;
+pub type VectorMachine<'p> = Machine<'p, VectorMemory, VectorStack, FakeAccount>;
 
-pub struct Machine<'p, M, S> {
-    available_gas: Gas,
-    pub pc: PC<'p>,
-    pub memory: M, // Contains i
-    pub stack: S,
-    pub account: Account,
+pub trait Machine {
+    type P: PC;
+    type M: Memory;
+    type Sta: Stack;
+    type Sto: Storage;
+    type T: Transaction;
+    type B: Block;
+    type Bc: Blockchain;
+
+    fn pc(&self) -> &P;
+    fn pc_mut(&mut self) -> &mut P;
+    fn memory(&self) -> &M;
+    fn memory_mut(&mut self) -> &mut M;
+    fn stack(&self) -> &Sta;
+    fn stack_mut(&mut self) -> &mut Sta;
+    fn storage(&self) -> &Sto;
+    fn storage_mut(&mut self) -> &mut Sto;
+
+    fn transaction(&self) -> &T;
+    fn block(&self) -> &B;
+    fn blockchain(&self) -> &Bc;
+    fn available_gas(&self) -> Gas;
 }
 
-impl<'p, M, S> Machine<'p, M, S> where M: Memory, S: Stack {
-    pub fn new(code: &'p [u8], data: &[u8], available_gas: Gas) -> Self {
-        Self {
+pub struct VectorMachine {
+    pc: VectorPC,
+    memory: VectorMemory,
+    stack: VectorStack,
+    storage: FakeStorage,
+    transaction: FakeTransaction,
+    block: FakeBlock,
+    blockchain: FakeBlockchain,
+    available_gas: Gas,
+}
+
+impl VectorMachine {
+    pub fn new(code: &[u8], data: &[u8], available_gas: Gas) -> VectorMachine {
+        VectorMachine {
+            pc: VectorPC::new(code),
+            memory: VectorMemory::new(),
+            stack: VectorStack::new(),
+            storage: FakeStorage::new(),
+            transaction: FakeTransaction::message_call(U256::zero(), data),
+            block: FakeBlock,
+            blockchain: FakeBlockchain,
             available_gas: available_gas,
-            pc: PC::new(code),
-            memory: M::new(),
-            stack: S::new(),
-            account: Account::default(),
         }
     }
+}
 
+impl Machine for VectorMachine {
+    type P = VectorPC;
+    type M = VectorMemory;
+    type Sta = VectorStack;
+    type Sto = FakeStorage;
+    type T = FakeTransaction;
+    type B = FakeBlock;
+    type Bc = FakeBlockchain;
+
+    fn pc(&self) -> &P {
+        &self.pc
+    }
+
+    fn pc_mut(&mut self) -> &mut P {
+        &mut self.pc
+    }
+
+    fn memory(&self) -> &M {
+        &self.memory
+    }
+
+    fn memory_mut(&mut self) -> &mut M {
+        &mut self.memory
+    }
+
+    fn stack(&self) -> &Sta {
+        &self.stack
+    }
+
+    fn stack_mut(&mut self) -> &mut Sta {
+        &mut self.stack
+    }
+
+    fn storage(&self) -> &Sto
+}
+
+impl<'p, M, S, A> Machine<'p, M, S, A> where M: Memory, S: Stack, A: Account {
     pub fn available_gas(&self) -> Gas {
         self.available_gas
     }
