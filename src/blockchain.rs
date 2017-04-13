d023daab7742d93c566d5a65935385244681cdd3
Wei Tang <hi@that.world>
2017-04-13 16:41:46 +0800

    Problem: No blockchain implementation Solution: Create traits Block, Blockchain, and add FakeBlock, FakeBlockchain

---
+++ src/blockchain.rs
@@ -0,0 +1,61 @@
+pub trait Block {
+    fn account_code(&self, address: Address) -> Option<&[u8]>;
+    fn coinbase(&self) -> Address;
+    fn balance(&self, address: Address) -> Option<U256>;
+    fn timestamp(&self) -> U256;
+    fn number(&self) -> U256;
+    fn difficulty(&self) -> U256;
+    fn gas_limit(&self) -> U256;
+}
+
+pub struct FakeBlock;
+
+impl Block for FakeBlock {
+    fn account_code(address: Address) -> Option<&[u8]> {
+        None
+    }
+
+    fn coinbase(&self) -> Address {
+        Address::default()
+    }
+
+    fn timestamp(&self) -> U256 {
+        U256::zero()
+    }
+
+    fn number(&self) -> U256 {
+        U256::zero()
+    }
+
+    fn difficulty(&self) -> U256 {
+        U256::zero()
+    }
+
+    fn gas_limit(&self) -> U256 {
+        U256::zero()
+    }
+}
+
+impl Default for FakeBlock {
+    fn default() -> FakeBlock {
+        FakeBlock
+    }
+}
+
+pub trait Blockchain {
+    fn blockhash(n: U256) -> H256;
+}
+
+pub struct FakeBlockchain;
+
+impl Blockchain for FakeBlockchain {
+    fn blockhash(n: U256) -> H256 {
+        H256::default()
+    }
+}
+
+impl Default for FakeBlockchain {
+    fn default() -> FakeBlockchain {
+        FakeBlockchain
+    }
+}
