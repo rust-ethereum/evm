c786b99edd8d7d474a530e13963a29f202c4eff9
Wei Tang <hi@that.world>
2017-04-13 16:44:25 +0800

    Problem: No account implementation Solution: Add Account trait and FakeAccount struct

--- src/account.rs
+++ src/account.rs
@@ -1,21 +1,104 @@
 use utils::u256::U256;
-use merkle::{MerkleTree};
-use crypto::sha3::Sha3;
+use utils::address::Address;
 
-pub struct Account {
-    nonce: usize,
-    balance: U256,
-    pub storageRoot: MerkleTree<U256>,
-    codeHash: U256,
+pub trait Account {
+    type Sto: Storage;
+
+    fn address(&self) -> Address;
+    fn balance(&self) -> U256;
+    fn storage(&self) -> &Sto;
+    fn storage_mut(&mut self) -> &mut Sto;
+    fn code(&self) -> Option<&[u8]>;
 }
 
-impl Default for Account {
-    fn default() -> Account {
-        Account {
-            nonce: 0,
-            balance: 0.into(),
-            storageRoot: MerkleTree::from_vec::<U256>(Sha3::keccak256(), Vec::new()),
-            codeHash: 0.into()
+pub struct FakeAccount {
+    address: Address,
+    nonce: usize,
+    balance: U256,
+    storage: FakeStorage,
+    code: Option<Vec<u8>>,
+}
+
+impl FakeAccount {
+    pub fn new(address: Address) -> FakeAccount {
+        FakeAccount::with_code(address, None);
+    }
+
+    pub fn with_code(address: Address, code: Option<&[u8]>) -> FakeAccount {
+        FakeAccount {
+            address: Address,
+            nounce: 0,
+            balance: U256::zero(),
+            storage: FakeStorage::new(),
+            code: code.map(|s| s.into())
+        }
+    }
+}
+
+impl Default for FakeAccount {
+    fn default() -> FakeAccount {
+        FakeAccount::new(Address::default())
+    }
+}
+
+impl Account for FakeAccount {
+    type Storage = FakeStorage;
+
+    fn address(&self) -> Address {
+        self.address
+    }
+
+    fn balance(&self) -> U256 {
+        self.balance
+    }
+
+    fn storage(&self) -> &Storage {
+        &self.storage
+    }
+
+    fn storage_mut(&self) -> &mut Storage {
+        &mut self.storage
+    }
+
+    fn code(&self) -> Option<&[u8]> {
+        self.code.map(|s| s.into())
+    }
+}
+
+pub trait Storage { // A word-addressable word array, similar to memory, and is not volatile.
+    fn write(&mut self, index: U256, value: U256);
+    fn read(&self, index: U256) -> U256;
+}
+
+pub struct FakeStorage {
+    storage: Vec<U256>,
+}
+
+impl FakeStorage {
+    pub fn new() -> FakeStorage {
+        FakeStorage {
+            storage: Vec::new(),
+        }
+    }
+}
+
+impl Storage for FakeStorage {
+    fn write(&mut self, index: U256, value: U256) {
+        let index: usize = index.into();
+
+        if self.storage.len() <= index {
+            self.storage.resize(index, 0.into());
+        }
+
+        self.storage[index] = value;
+    }
+
+    fn read(&self, index: U256) -> U256 {
+        let index: usize = index.into();
+
+        match self.storage.get(index) {
+            Some(&v) => v,
+            None => U256::zero()
         }
     }
 }
