f8b93d2932ffc025dfe44eb08a60be81fa73cd1c
Wei Tang <hi@that.world>
2017-04-13 19:25:46 +0800

    Problem: blockchain does not compile Solution: fix mistakes

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,3 +1,8 @@
+use utils::u256::U256;
+use utils::gas::Gas;
+use utils::address::Address;
+use utils::hash::H256;
+
 pub trait Block {
     fn account_code(&self, address: Address) -> Option<&[u8]>;
     fn coinbase(&self) -> Address;
@@ -5,18 +10,22 @@ pub trait Block {
     fn timestamp(&self) -> U256;
     fn number(&self) -> U256;
     fn difficulty(&self) -> U256;
-    fn gas_limit(&self) -> U256;
+    fn gas_limit(&self) -> Gas;
 }
 
 pub struct FakeBlock;
 
 impl Block for FakeBlock {
-    fn account_code(address: Address) -> Option<&[u8]> {
+    fn account_code(&self, address: Address) -> Option<&[u8]> {
         None
     }
 
     fn coinbase(&self) -> Address {
         Address::default()
+    }
+
+    fn balance(&self, address: Address) -> Option<U256> {
+        None
     }
 
     fn timestamp(&self) -> U256 {
@@ -31,8 +40,8 @@ impl Block for FakeBlock {
         U256::zero()
     }
 
-    fn gas_limit(&self) -> U256 {
-        U256::zero()
+    fn gas_limit(&self) -> Gas {
+        Gas::zero()
     }
 }
 
