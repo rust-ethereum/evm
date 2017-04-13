f0e230d026f1dd724b6bd333b41b126b7eaf8421
Wei Tang <hi@that.world>
2017-04-13 19:24:30 +0800

    Problem: Account results in circular dependency Solution: remove Account struct

--- src/account.rs
+++ src/account.rs
@@ -1,88 +1,30 @@
 use utils::u256::U256;
 use utils::address::Address;
 
-pub trait Account {
-    type Sto: Storage;
-
-    fn address(&self) -> Address;
-    fn balance(&self) -> U256;
-    fn storage(&self) -> &Sto;
-    fn storage_mut(&mut self) -> &mut Sto;
-    fn code(&self) -> Option<&[u8]>;
-}
-
-pub struct FakeAccount {
-    address: Address,
-    nonce: usize,
-    balance: U256,
-    storage: FakeStorage,
-    code: Option<Vec<u8>>,
-}
-
-impl FakeAccount {
-    pub fn new(address: Address) -> FakeAccount {
-        FakeAccount::with_code(address, None);
-    }
-
-    pub fn with_code(address: Address, code: Option<&[u8]>) -> FakeAccount {
-        FakeAccount {
-            address: Address,
-            nounce: 0,
-            balance: U256::zero(),
-            storage: FakeStorage::new(),
-            code: code.map(|s| s.into())
-        }
-    }
-}
-
-impl Default for FakeAccount {
-    fn default() -> FakeAccount {
-        FakeAccount::new(Address::default())
-    }
-}
-
-impl Account for FakeAccount {
-    type Storage = FakeStorage;
-
-    fn address(&self) -> Address {
-        self.address
-    }
-
-    fn balance(&self) -> U256 {
-        self.balance
-    }
-
-    fn storage(&self) -> &Storage {
-        &self.storage
-    }
-
-    fn storage_mut(&self) -> &mut Storage {
-        &mut self.storage
-    }
-
-    fn code(&self) -> Option<&[u8]> {
-        self.code.map(|s| s.into())
-    }
-}
-
 pub trait Storage { // A word-addressable word array, similar to memory, and is not volatile.
     fn write(&mut self, index: U256, value: U256);
     fn read(&self, index: U256) -> U256;
 }
 
-pub struct FakeStorage {
+pub struct VectorStorage {
     storage: Vec<U256>,
 }
 
-impl FakeStorage {
-    pub fn new() -> FakeStorage {
-        FakeStorage {
+impl VectorStorage {
+    pub fn new() -> VectorStorage {
+        VectorStorage {
             storage: Vec::new(),
+        }
+    }
+
+    pub fn with_storage(storage: &[U256]) -> VectorStorage {
+        VectorStorage {
+            storage: storage.into(),
         }
     }
 }
 
-impl Storage for FakeStorage {
+impl Storage for VectorStorage {
     fn write(&mut self, index: U256, value: U256) {
         let index: usize = index.into();
 
