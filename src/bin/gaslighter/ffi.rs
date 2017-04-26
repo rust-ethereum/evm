03fb9520b9ec756d8148e9571b34a3d161d67348
Stewart Mackenzie <setori88@gmail.com>
2017-04-27 01:11:29 +0800

    Problem: vm wasn't firing
    Solution: make it fire via ffi

--- src/bin/gaslighter/ffi.rs
+++ src/bin/gaslighter/ffi.rs
@@ -3,8 +3,6 @@ use sputnikvm::{SputnikVM};
 use serde_json::{Value, Error};
 use std::io::{BufReader, Write, stdout};
 
-
-
 pub fn test_ffi_transaction(name: &str, v: &Value, debug: bool, sputnikvm_path: &str) {
     let mut ffi = FFI(libloading::Library::new(sputnikvm_path).unwrap_or_else(|error| panic!("{}",error)));
     println!("Testing {} ... ", name);
@@ -14,18 +12,27 @@ pub fn test_ffi_transaction(name: &str, v: &Value, debug: bool, sputnikvm_path: 
     stdout().flush();
 
     let svm = ffi.sputnikvm_new(v);
-    // ffi.sputnikvm_free(svm);
+    ffi.sputnikvm_fire(svm);
+    ffi.sputnikvm_free(svm);
 }
 
 struct FFI(libloading::Library);
 
 impl FFI {
-    fn sputnikvm_new(&self, v: &Value) {
+    fn sputnikvm_new(&self, v: &Value) -> *mut SputnikVM {
         unsafe {
-            let f = self.0.get::<fn(v: &Value)> (
+            let f = self.0.get::<fn(v: &Value) -> *mut SputnikVM > (
                 b"sputnikvm_new\0"
             ).unwrap();
             f(v)
+        }
+    }
+    fn sputnikvm_fire(&self, svm: *mut SputnikVM) {
+        unsafe {
+            let f = self.0.get::<fn(svm: *mut SputnikVM)> (
+                b"sputnikvm_fire\0"
+            ).unwrap();
+            f(svm)
         }
     }
     fn sputnikvm_free(&self, svm: *mut SputnikVM) {
