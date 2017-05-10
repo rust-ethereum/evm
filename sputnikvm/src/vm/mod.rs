70301600f593f6f277728140ff9e4bb9a7af5708
Wei Tang <hi@that.world>
2017-05-10 21:47:39 +0800

    Problem: no commit fn in VM Solution: add it

--- sputnikvm/src/vm/mod.rs
+++ sputnikvm/src/vm/mod.rs
@@ -15,11 +15,26 @@ pub use self::params::{Context, BlockHeader, Log, Patch};
 pub use self::eval::{State, Machine, Status};
 pub use self::commit::{AccountCommitment, Account};
 
-use self::errors::RequireError;
+use utils::bigint::M256;
+use self::errors::{RequireError, CommitError};
 
 pub struct VM<M, S>(Vec<Machine<M, S>>);
 
 impl<M: Memory + Default, S: Storage + Default + Clone> VM<M, S> {
+    pub fn commit_account(&mut self, commitment: AccountCommitment<S>) -> Result<(), CommitError> {
+        for machine in &mut self.0 {
+            machine.commit_account(commitment.clone())?;
+        }
+        Ok(())
+    }
+
+    pub fn commit_blockhash(&mut self, number: M256, hash: M256) -> Result<(), CommitError> {
+        for machine in &mut self.0 {
+            machine.commit_blockhash(number, hash)?;
+        }
+        Ok(())
+    }
+
     pub fn step(&mut self) -> Result<(), RequireError> {
         unimplemented!()
     }
