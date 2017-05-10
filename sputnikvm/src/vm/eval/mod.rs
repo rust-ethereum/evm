d12ff8bac046a0f8eee92b7d1ff4d3981cd4b33d
Wei Tang <hi@that.world>
2017-05-10 21:43:53 +0800

    Problem: no account commitment in Machine Solution: add it

--- sputnikvm/src/vm/eval/mod.rs
+++ sputnikvm/src/vm/eval/mod.rs
@@ -1,8 +1,8 @@
 use utils::bigint::M256;
 use utils::gas::Gas;
 use super::commit::{AccountState, BlockhashState};
-use super::errors::{RequireError, MachineError};
-use super::{Stack, Context, BlockHeader, Patch, PC, Storage, Memory};
+use super::errors::{RequireError, MachineError, CommitError};
+use super::{Stack, Context, BlockHeader, Patch, PC, Storage, Memory, AccountCommitment};
 
 pub mod cost;
 pub mod run;
@@ -39,6 +39,14 @@ pub enum Status {
 }
 
 impl<M: Memory + Default, S: Storage + Default + Clone> Machine<M, S> {
+    pub fn commit_account(&mut self, commitment: AccountCommitment<S>) -> Result<(), CommitError> {
+        self.state.account_state.commit(commitment)
+    }
+
+    pub fn commit_blockhash(&mut self, number: M256, hash: M256) -> Result<(), CommitError> {
+        self.state.blockhash_state.commit(number, hash)
+    }
+
     pub fn step(&mut self) -> Result<(), RequireError> {
         unimplemented!()
     }
