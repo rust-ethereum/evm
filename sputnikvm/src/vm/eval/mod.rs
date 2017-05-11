d496e865a2d392d6f9c9cfc885813bd2ca7c66ac
Wei Tang <hi@that.world>
2017-05-11 22:43:40 +0800

    Problem: available/after_gas not clear in step Solution: tidy up the code

--- sputnikvm/src/vm/eval/mod.rs
+++ sputnikvm/src/vm/eval/mod.rs
@@ -168,21 +168,22 @@ impl<M: Memory + Default, S: Storage + Default + Clone> Machine<M, S> {
         };
 
         let instruction = self.pc.peek().unwrap();
+        let position = self.pc.position();
         let memory_cost = memory_cost(instruction, &self.state);
         let memory_gas = memory_gas(memory_cost);
         let gas_cost = gas_cost(instruction, &self.state);
         let gas_stipend = gas_stipend(instruction, &self.state);
         let gas_refund = gas_refund(instruction, &self.state);
 
-        if self.state.context.gas_limit < self.state.used_gas + self.state.memory_gas() + memory_gas + gas_cost {
+        if self.state.available_gas() < memory_gas + gas_cost {
             self.status = MachineStatus::ExitedErr(MachineError::EmptyGas);
             return Ok(());
         }
 
         let instruction = self.pc.read().unwrap();
-        let available_gas = self.state.available_gas();
-        let result = run_opcode((instruction, self.pc.position()),
-                                &mut self.state, gas_stipend, available_gas);
+        let after_gas = self.state.available_gas() - memory_gas - gas_cost;
+        let result = run_opcode((instruction, position),
+                                &mut self.state, gas_stipend, after_gas);
 
         self.state.used_gas = self.state.used_gas + gas_cost;
         self.state.memory_cost = memory_cost;
