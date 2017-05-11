d4d6fa841c812064a053512be6d0f3b0fadfaa45
Wei Tang <hi@that.world>
2017-05-11 22:34:45 +0800

    Problem: some tests are not present Solution: add some of them back

---
+++ jsontests/tests/log.rs
@@ -0,0 +1,59 @@
+extern crate jsontests;
+extern crate serde_json;
+#[macro_use]
+extern crate lazy_static;
+
+use serde_json::Value;
+use jsontests::test_transaction;
+
+lazy_static! {
+    static ref TESTS: Value =
+        serde_json::from_str(include_str!("files/vmLogTest.json")).unwrap();
+}
+
+#[test] fn log0_emptyMem() { assert_eq!(test_transaction("log0_emptyMem", &TESTS["log0_emptyMem"], true), true); }
+#[test] fn log0_logMemStartTooHigh() { assert_eq!(test_transaction("log0_logMemStartTooHigh", &TESTS["log0_logMemStartTooHigh"], true), true); }
+#[test] fn log0_logMemsizeTooHigh() { assert_eq!(test_transaction("log0_logMemsizeTooHigh", &TESTS["log0_logMemsizeTooHigh"], true), true); }
+#[test] fn log0_logMemsizeZero() { assert_eq!(test_transaction("log0_logMemsizeZero", &TESTS["log0_logMemsizeZero"], true), true); }
+#[test] fn log0_nonEmptyMem() { assert_eq!(test_transaction("log0_nonEmptyMem", &TESTS["log0_nonEmptyMem"], true), true); }
+#[test] fn log0_nonEmptyMem_logMemSize1() { assert_eq!(test_transaction("log0_nonEmptyMem_logMemSize1", &TESTS["log0_nonEmptyMem_logMemSize1"], true), true); }
+#[test] fn log0_nonEmptyMem_logMemSize1_logMemStart31() { assert_eq!(test_transaction("log0_nonEmptyMem_logMemSize1_logMemStart31", &TESTS["log0_nonEmptyMem_logMemSize1_logMemStart31"], true), true); }
+#[test] fn log1_Caller() { assert_eq!(test_transaction("log1_Caller", &TESTS["log1_Caller"], true), true); }
+#[test] fn log1_MaxTopic() { assert_eq!(test_transaction("log1_MaxTopic", &TESTS["log1_MaxTopic"], true), true); }
+#[test] fn log1_emptyMem() { assert_eq!(test_transaction("log1_emptyMem", &TESTS["log1_emptyMem"], true), true); }
+#[test] fn log1_logMemStartTooHigh() { assert_eq!(test_transaction("log1_logMemStartTooHigh", &TESTS["log1_logMemStartTooHigh"], true), true); }
+#[test] fn log1_logMemsizeTooHigh() { assert_eq!(test_transaction("log1_logMemsizeTooHigh", &TESTS["log1_logMemsizeTooHigh"], true), true); }
+#[test] fn log1_logMemsizeZero() { assert_eq!(test_transaction("log1_logMemsizeZero", &TESTS["log1_logMemsizeZero"], true), true); }
+#[test] fn log1_nonEmptyMem() { assert_eq!(test_transaction("log1_nonEmptyMem", &TESTS["log1_nonEmptyMem"], true), true); }
+#[test] fn log1_nonEmptyMem_logMemSize1() { assert_eq!(test_transaction("log1_nonEmptyMem_logMemSize1", &TESTS["log1_nonEmptyMem_logMemSize1"], true), true); }
+#[test] fn log1_nonEmptyMem_logMemSize1_logMemStart31() { assert_eq!(test_transaction("log1_nonEmptyMem_logMemSize1_logMemStart31", &TESTS["log1_nonEmptyMem_logMemSize1_logMemStart31"], true), true); }
+#[test] fn log2_Caller() { assert_eq!(test_transaction("log2_Caller", &TESTS["log2_Caller"], true), true); }
+#[test] fn log2_MaxTopic() { assert_eq!(test_transaction("log2_MaxTopic", &TESTS["log2_MaxTopic"], true), true); }
+#[test] fn log2_emptyMem() { assert_eq!(test_transaction("log2_emptyMem", &TESTS["log2_emptyMem"], true), true); }
+#[test] fn log2_logMemStartTooHigh() { assert_eq!(test_transaction("log2_logMemStartTooHigh", &TESTS["log2_logMemStartTooHigh"], true), true); }
+#[test] fn log2_logMemsizeTooHigh() { assert_eq!(test_transaction("log2_logMemsizeTooHigh", &TESTS["log2_logMemsizeTooHigh"], true), true); }
+#[test] fn log2_logMemsizeZero() { assert_eq!(test_transaction("log2_logMemsizeZero", &TESTS["log2_logMemsizeZero"], true), true); }
+#[test] fn log2_nonEmptyMem() { assert_eq!(test_transaction("log2_nonEmptyMem", &TESTS["log2_nonEmptyMem"], true), true); }
+#[test] fn log2_nonEmptyMem_logMemSize1() { assert_eq!(test_transaction("log2_nonEmptyMem_logMemSize1", &TESTS["log2_nonEmptyMem_logMemSize1"], true), true); }
+#[test] fn log2_nonEmptyMem_logMemSize1_logMemStart31() { assert_eq!(test_transaction("log2_nonEmptyMem_logMemSize1_logMemStart31", &TESTS["log2_nonEmptyMem_logMemSize1_logMemStart31"], true), true); }
+#[test] fn log3_Caller() { assert_eq!(test_transaction("log3_Caller", &TESTS["log3_Caller"], true), true); }
+#[test] fn log3_MaxTopic() { assert_eq!(test_transaction("log3_MaxTopic", &TESTS["log3_MaxTopic"], true), true); }
+#[test] fn log3_PC() { assert_eq!(test_transaction("log3_PC", &TESTS["log3_PC"], true), true); }
+#[test] fn log3_emptyMem() { assert_eq!(test_transaction("log3_emptyMem", &TESTS["log3_emptyMem"], true), true); }
+#[test] fn log3_logMemStartTooHigh() { assert_eq!(test_transaction("log3_logMemStartTooHigh", &TESTS["log3_logMemStartTooHigh"], true), true); }
+#[test] fn log3_logMemsizeTooHigh() { assert_eq!(test_transaction("log3_logMemsizeTooHigh", &TESTS["log3_logMemsizeTooHigh"], true), true); }
+#[test] fn log3_logMemsizeZero() { assert_eq!(test_transaction("log3_logMemsizeZero", &TESTS["log3_logMemsizeZero"], true), true); }
+#[test] fn log3_nonEmptyMem() { assert_eq!(test_transaction("log3_nonEmptyMem", &TESTS["log3_nonEmptyMem"], true), true); }
+#[test] fn log3_nonEmptyMem_logMemSize1() { assert_eq!(test_transaction("log3_nonEmptyMem_logMemSize1", &TESTS["log3_nonEmptyMem_logMemSize1"], true), true); }
+#[test] fn log3_nonEmptyMem_logMemSize1_logMemStart31() { assert_eq!(test_transaction("log3_nonEmptyMem_logMemSize1_logMemStart31", &TESTS["log3_nonEmptyMem_logMemSize1_logMemStart31"], true), true); }
+#[test] fn log4_Caller() { assert_eq!(test_transaction("log4_Caller", &TESTS["log4_Caller"], true), true); }
+#[test] fn log4_MaxTopic() { assert_eq!(test_transaction("log4_MaxTopic", &TESTS["log4_MaxTopic"], true), true); }
+#[test] fn log4_PC() { assert_eq!(test_transaction("log4_PC", &TESTS["log4_PC"], true), true); }
+#[test] fn log4_emptyMem() { assert_eq!(test_transaction("log4_emptyMem", &TESTS["log4_emptyMem"], true), true); }
+#[test] fn log4_logMemStartTooHigh() { assert_eq!(test_transaction("log4_logMemStartTooHigh", &TESTS["log4_logMemStartTooHigh"], true), true); }
+#[test] fn log4_logMemsizeTooHigh() { assert_eq!(test_transaction("log4_logMemsizeTooHigh", &TESTS["log4_logMemsizeTooHigh"], true), true); }
+#[test] fn log4_logMemsizeZero() { assert_eq!(test_transaction("log4_logMemsizeZero", &TESTS["log4_logMemsizeZero"], true), true); }
+#[test] fn log4_nonEmptyMem() { assert_eq!(test_transaction("log4_nonEmptyMem", &TESTS["log4_nonEmptyMem"], true), true); }
+#[test] fn log4_nonEmptyMem_logMemSize1() { assert_eq!(test_transaction("log4_nonEmptyMem_logMemSize1", &TESTS["log4_nonEmptyMem_logMemSize1"], true), true); }
+#[test] fn log4_nonEmptyMem_logMemSize1_logMemStart31() { assert_eq!(test_transaction("log4_nonEmptyMem_logMemSize1_logMemStart31", &TESTS["log4_nonEmptyMem_logMemSize1_logMemStart31"], true), true); }
+#[test] fn log_2logs() { assert_eq!(test_transaction("log_2logs", &TESTS["log_2logs"], true), true); }
---
+++ jsontests/tests/sha3.rs
@@ -0,0 +1,31 @@
+extern crate jsontests;
+extern crate serde_json;
+#[macro_use]
+extern crate lazy_static;
+
+use serde_json::Value;
+use jsontests::test_transaction;
+
+lazy_static! {
+    static ref TESTS: Value =
+        serde_json::from_str(include_str!("files/vmSha3Test.json")).unwrap();
+}
+
+#[test] fn sha3_0() { assert_eq!(test_transaction("sha3_0", &TESTS["sha3_0"], true), true); }
+#[test] fn sha3_1() { assert_eq!(test_transaction("sha3_1", &TESTS["sha3_1"], true), true); }
+#[test] fn sha3_2() { assert_eq!(test_transaction("sha3_2", &TESTS["sha3_2"], true), true); }
+#[test] fn sha3_3() { assert_eq!(test_transaction("sha3_3", &TESTS["sha3_3"], true), true); }
+#[test] fn sha3_4() { assert_eq!(test_transaction("sha3_4", &TESTS["sha3_4"], true), true); }
+#[test] fn sha3_5() { assert_eq!(test_transaction("sha3_5", &TESTS["sha3_5"], true), true); }
+#[test] fn sha3_6() { assert_eq!(test_transaction("sha3_6", &TESTS["sha3_6"], true), true); }
+#[test] fn sha3_bigOffset() { assert_eq!(test_transaction("sha3_bigOffset", &TESTS["sha3_bigOffset"], true), true); }
+#[test] fn sha3_bigOffset2() { assert_eq!(test_transaction("sha3_bigOffset2", &TESTS["sha3_bigOffset2"], true), true); }
+#[test] fn sha3_bigSize() { assert_eq!(test_transaction("sha3_bigSize", &TESTS["sha3_bigSize"], true), true); }
+#[test] fn sha3_memSizeNoQuadraticCost31() { assert_eq!(test_transaction("sha3_memSizeNoQuadraticCost31", &TESTS["sha3_memSizeNoQuadraticCost31"], true), true); }
+#[test] fn sha3_memSizeQuadraticCost32() { assert_eq!(test_transaction("sha3_memSizeQuadraticCost32", &TESTS["sha3_memSizeQuadraticCost32"], true), true); }
+#[test] fn sha3_memSizeQuadraticCost32_zeroSize() { assert_eq!(test_transaction("sha3_memSizeQuadraticCost32_zeroSize", &TESTS["sha3_memSizeQuadraticCost32_zeroSize"], true), true); }
+#[test] fn sha3_memSizeQuadraticCost33() { assert_eq!(test_transaction("sha3_memSizeQuadraticCost33", &TESTS["sha3_memSizeQuadraticCost33"], true), true); }
+#[test] fn sha3_memSizeQuadraticCost63() { assert_eq!(test_transaction("sha3_memSizeQuadraticCost63", &TESTS["sha3_memSizeQuadraticCost63"], true), true); }
+#[test] fn sha3_memSizeQuadraticCost64() { assert_eq!(test_transaction("sha3_memSizeQuadraticCost64", &TESTS["sha3_memSizeQuadraticCost64"], true), true); }
+#[test] fn sha3_memSizeQuadraticCost64_2() { assert_eq!(test_transaction("sha3_memSizeQuadraticCost64_2", &TESTS["sha3_memSizeQuadraticCost64_2"], true), true); }
+#[test] fn sha3_memSizeQuadraticCost65() { assert_eq!(test_transaction("sha3_memSizeQuadraticCost65", &TESTS["sha3_memSizeQuadraticCost65"], true), true); }
