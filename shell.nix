edbb8db168be2e10ffe529ad1d62ffe07e88936c
Wei Tang <hi@that.world>
2017-05-11 18:50:51 +0800

    Problem: nix-shell was removed Solution: only add the env back

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ pkgs ? (
+  let
+    nixpkgs = import <nixpkgs>;
+    pkgs_ = (nixpkgs {});
+    rustOverlay = (pkgs_.fetchFromGitHub {
+      owner = "mozilla";
+      repo = "nixpkgs-mozilla";
+      rev = "e2a920faec5a9ebd6ff34abf072aacb4e0ed6f70";
+      sha256 = "1lq7zg388y4wrbl165wraji9dmlb8rkjaiam9bq28n3ynsp4b6fz";
+    });
+  in (nixpkgs {
+    overlays = [
+      (import (builtins.toPath "${rustOverlay}/rust-overlay.nix"))
+      (self: super: {
+        rust = {
+          rustc = super.rustChannels.stable.rust;
+          cargo = super.rustChannels.stable.cargo;
+        };
+        rustPlatform = super.recurseIntoAttrs (super.makeRustPlatform {
+          rustc = super.rustChannels.stable.rust;
+          cargo = super.rustChannels.stable.cargo;
+        });
+      })
+    ];
+  }))
+}:
+
+with pkgs;
+
+stdenv.mkDerivation {
+  name = "sputnikvm-env";
+  buildInputs = [
+    rustc cargo gdb
+  ];
+}
