f8758c4915c40824ff0201f8b8a494b8418b53d9
Wei Tang <hi@that.world>
2017-05-29 15:29:11 +0800

    Problem: new feature
    Solution: geth RPC implementation using serde and hyper

--- blockchain_geth/src/lib.rs
+++ blockchain_geth/src/lib.rs
@@ -1,133 +1,135 @@
+extern crate serde;
 extern crate serde_json;
+extern crate hyper;
+
+#[macro_use]
+extern crate serde_derive;
 
 use std::process::{Command};
+use std::io::Read;
+use hyper::header::ContentType;
+use hyper::client::Client;
 use serde_json::Value;
 
-fn code(tx_to: &str, tx_from: &str, blk_no: &str, address: &str) {
-    let prefix = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getCode\",\"params\":[\"";
-    let postfix = "\"],\"id\":1}";
-    let together = format!("{}{}\", \"{}{}", prefix, tx_to, blk_no, postfix);
-    let mut cmd = Command::new("curl")
-        .arg("-X")
-        .arg("POST")
-        .arg("--data")
-        .arg(together)
-        .arg(address)
-        .output()
-        .unwrap();
-    let serialized = String::from_utf8_lossy(&cmd.stdout);
-    let v: Value = serde_json::from_str(&serialized).unwrap();
-    match v["result"].as_str() {
-        Some(code) => {
-            match code {
-                "0x" => {},
-                _ => {
-                    let blk_no = i64::from_str_radix(&blk_no[2..], 16);
-                    print!("blk_no: {:?}, code: {:?}\n", blk_no, code );
-                }
-            }
-        },
-        None => {},
-    };
+#[derive(Serialize, Deserialize)]
+struct RPCRequest {
+    jsonrpc: String,
+    method: String,
+    params: Vec<String>,
+    id: usize,
 }
 
-fn transaction_by_block_number_and_index(blk_no: &str, tx_index: &str, address: &str) {
-    let prefix = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionByBlockNumberAndIndex\",\"params\":[\"";
-    let postfix = "\"],\"id\":1}";
-    let together = format!("{}{}\",\"{}{}", prefix, blk_no, tx_index, postfix);
-    let mut cmd = Command::new("curl")
-        .arg("-X")
-        .arg("POST")
-        .arg("--data")
-        .arg(together)
-        .arg(address)
-        .output()
-        .unwrap();
-    let serialized = String::from_utf8_lossy(&cmd.stdout);
-    let v: Value = serde_json::from_str(&serialized).unwrap();
-    let to = match v["result"]["to"].as_str() {
-        Some(s) => {s},
-        None => { return }, // if it's null it means a contract was created
-    };
-    let from = v["result"]["from"].as_str().unwrap();
-    let blk_no = v["result"]["blockNumber"].as_str().unwrap();
-    code(to, from, blk_no, address);
+#[derive(Serialize, Deserialize)]
+struct RPCResponse<T> {
+    jsonrpc: String,
+    result: T,
+    id: usize,
 }
 
-fn transaction_count(blk_no: &str, address: &str) {
-    let prefix = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockTransactionCountByNumber\",\"params\":[\"";
-    let postfix = "\"],\"id\":1}";
-    let together = format!("{}{}{}", prefix, blk_no, postfix);
-    let mut cmd = Command::new("curl")
-        .arg("-X")
-        .arg("POST")
-        .arg("--data")
-        .arg(together)
-        .arg(address)
-        .output()
-        .unwrap();
-    let serialized = String::from_utf8_lossy(&cmd.stdout);
-    let v: Value = serde_json::from_str(&serialized).unwrap();
-    match v["result"].as_str() {
-        Some(tx_count) => {
-            match tx_count {
-                "0x0" => {},
-                _ => {
-                    let tx_count = i64::from_str_radix(&tx_count[2..], 16);
-                    for index in 0..tx_count.unwrap() {
-                        transaction_by_block_number_and_index(blk_no, format!("0x{:x}", index).as_str(), address)
-                    }
-                }
-            }
-        },
-        None => {},
-    };
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RPCTransaction {
+    pub hash: String,
+    pub nonce: String,
+    pub blockHash: String,
+    pub blockNumber: String,
+    pub transactionIndex: String,
+    pub from: String,
+    pub to: String,
+    pub value: String,
+    pub gas: String,
+    pub gasPrice: String,
+    pub input: String,
 }
 
-fn block(blk_no: &str, address: &str) {
-    let prefix = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"";
-    let postfix = "\", true],\"id\":1}";
-    let together = format!("{}{}{}", prefix, blk_no, postfix);
-    let mut cmd = Command::new("curl")
-        .arg("-X")
-        .arg("POST")
-        .arg("--data")
-        .arg(together)
-        .arg(address)
-        .output()
-        .unwrap();
-    let serialized = String::from_utf8_lossy(&cmd.stdout);
-    let v: Value = serde_json::from_str(&serialized).unwrap();
-    match v["result"]["transactionsRoot"].as_str() {
-        Some(p) => {
-            transaction_count(blk_no, address);
-        },
-        None => {},
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RPCBlock {
+    pub number: String,
+    pub hash: String,
+    pub parentHash: String,
+    pub nonce: String,
+    pub sha3Uncles: String,
+    pub logsBloom: String,
+    pub transactionsRoot: String,
+    pub stateRoot: String,
+    pub miner: String,
+    pub difficulty: String,
+    pub totalDifficulty: String,
+    pub extraData: String,
+    pub size: String,
+    pub gasLimit: String,
+    pub gasUsed: String,
+    pub timestamp: String,
+    pub transactions: Vec<String>,
+    pub uncles: Vec<String>
+}
+
+pub struct GethRPCClient {
+    endpoint: String,
+    free_id: usize,
+}
+
+impl GethRPCClient {
+    pub fn new(endpoint: &str) -> Self {
+        GethRPCClient {
+            endpoint: endpoint.to_string(),
+            free_id: 1,
+        }
+    }
+
+    fn rpc_request<T: serde::Deserialize>(&mut self, method: &str, params: Vec<String>) -> T {
+        let request = RPCRequest {
+            jsonrpc: "2.0".to_string(),
+            method: method.to_string(),
+            params: params,
+            id: self.free_id,
+        };
+        self.free_id = self.free_id + 1;
+
+        let client = Client::new();
+        let mut response_raw = client.post(&self.endpoint)
+            .header(ContentType::json())
+            .body(&serde_json::to_string(&request).unwrap())
+            .send().unwrap();
+        let mut buffer = String::new();
+        response_raw.read_to_string(&mut buffer).unwrap();
+
+        let response: RPCResponse<T> = serde_json::from_str(&buffer).unwrap();
+        response.result
+    }
+
+    pub fn get_block_transaction_count_by_number(&mut self, number: &str) -> String {
+        self.rpc_request::<String>("eth_getBlockTransactionCountByNumber",
+                                   vec![number.to_string()])
+    }
+
+    pub fn get_transaction_by_block_number_and_index(&mut self, number: &str, index: &str)
+                                                 -> RPCTransaction {
+        self.rpc_request::<RPCTransaction>("eth_getTransactionByBlockNumberAndIndex",
+                                           vec![number.to_string(), index.to_string()])
+    }
+
+    pub fn get_block_by_number(&mut self, number: &str) -> RPCBlock {
+        self.rpc_request::<RPCBlock>("eth_getBlockByNumber",
+                                     vec![number.to_string(), "false".to_string()])
+    }
+
+    pub fn block_number(&mut self) -> String {
+        self.rpc_request::<String>("eth_blockNumber", vec![])
+    }
+
+    pub fn get_code(&mut self, address: &str, number: &str) -> String {
+        self.rpc_request::<String>("eth_getCode", vec![address.to_string(), number.to_string()])
     }
 }
 
-fn height(address: &str) -> i64 {
-    let mut cmd = Command::new("curl")
-        .arg("-X")
-        .arg("POST")
-        .arg("--data")
-        .arg("{\"jsonrpc\":\"2.0\",\"method\":\"eth_blockNumber\",\"params\":[],\"id\":83}")
-        .arg(address)
-        .output()
-        .unwrap();
-    let serialized = String::from_utf8_lossy(&cmd.stdout);
-    let v: Value = serde_json::from_str(&serialized).unwrap();
-    let raw = v["result"].as_str().unwrap();
-    let z = i64::from_str_radix(&raw[2..], 16);
-    z.unwrap()
-}
-
 pub fn regression(address: &str) -> bool {
-    let height = height(address);
+    let mut client = GethRPCClient::new("http://localhost:8545");
+    let height = usize::from_str_radix(&client.block_number(), 16).unwrap();
     println!("height: {}", height);
-    // blk_no 49439 is when transactions to code starts appearing.
+
     for blk_no in 49439..height {
-        block(format!("0x{:x}", blk_no).as_str(), address);
-    };
+        let block = client.get_block_by_number(format!("0x{:x}", blk_no).as_str());
+        println!("{:?}", block);
+    }
     false
 }
