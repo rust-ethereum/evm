999e86dc357fcd186533fa6d36581b9b44b13b4c
Wei Tang <hi@that.world>
2017-05-29 19:12:57 +0800

    Problem: new feature
    Solution: most of the Ethereum RPC calls in gethrpc

--- gethrpc/src/lib.rs
+++ gethrpc/src/lib.rs
@@ -63,6 +63,37 @@ pub struct RPCBlock {
     pub uncles: Vec<String>
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum RPCSyncStatus {
+    NotSync(bool),
+    Sync {
+        startingBlock: String,
+        currentBlock: String,
+        highestBlock: String,
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RPCTransactionReceipt {
+    pub transactionHash: String,
+    pub transactionIndex: String,
+    pub blockHash: String,
+    pub blockNumber: String,
+    pub cumulativeGasUsed: String,
+    pub gasUsed: String,
+    pub contractAddress: String,
+    pub logs: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RPCFilter {
+    pub fromBlock: String,
+    pub toBlock: String,
+    pub address: String,
+    pub topics: Vec<String>,
+}
+
 pub struct GethRPCClient {
     endpoint: String,
     free_id: usize,
@@ -97,9 +128,135 @@ impl GethRPCClient {
         response.result
     }
 
+    pub fn client_version(&mut self) -> String {
+        self.rpc_request::<String>("web3_clientVersion", vec![])
+    }
+
+    pub fn net_version(&mut self) -> String {
+        self.rpc_request::<String>("net_version", vec![])
+    }
+
+    pub fn net_listening(&mut self) -> bool {
+        self.rpc_request::<bool>("net_listening", vec![])
+    }
+
+    pub fn net_peer_count(&mut self) -> String {
+        self.rpc_request::<String>("net_peerCount", vec![])
+    }
+
+    pub fn sha3(&mut self, data: &str) -> String {
+        self.rpc_request::<String>("web3_sha3", vec![])
+    }
+
+    pub fn protocol_version(&mut self) -> String {
+        self.rpc_request::<String>("eth_protocolVersion", vec![])
+    }
+
+    pub fn syncing(&mut self) -> RPCSyncStatus {
+        self.rpc_request::<RPCSyncStatus>("eth_syncing", vec![])
+    }
+
+    pub fn coinbase(&mut self) -> String {
+        self.rpc_request::<String>("eth_coinbase", vec![])
+    }
+
+    pub fn mining(&mut self) -> bool {
+        self.rpc_request::<bool>("eth_mining", vec![])
+    }
+
+    pub fn hashrate(&mut self) -> String {
+        self.rpc_request::<String>("eth_hashrate", vec![])
+    }
+
+    pub fn gas_price(&mut self) -> String {
+        self.rpc_request::<String>("eth_gasPrice", vec![])
+    }
+
+    pub fn accounts(&mut self) -> Vec<String> {
+        self.rpc_request::<Vec<String>>("eth_accounts", vec![])
+    }
+
+    pub fn block_number(&mut self) -> String {
+        self.rpc_request::<String>("eth_blockNumber", vec![])
+    }
+
+    pub fn get_balance(&mut self, address: &str, number: &str) -> String {
+        self.rpc_request::<String>("eth_getBalance",
+                                   vec![address.to_string(), number.to_string()])
+    }
+
+    pub fn get_storage_at(&mut self, address: &str, index: &str, number: &str) -> String {
+        self.rpc_request::<String>("eth_getStorageAt",
+                                   vec![address.to_string(), index.to_string(), number.to_string()])
+    }
+
+    pub fn get_transaction_count(&mut self, address: &str, number: &str) -> String {
+        self.rpc_request::<String>("eth_getTransactionCount",
+                                   vec![address.to_string(), number.to_string()])
+    }
+
+    pub fn get_block_transaction_count_by_hash(&mut self, hash: &str) -> String {
+        self.rpc_request::<String>("eth_getBlockTransactionCountByHash",
+                                   vec![hash.to_string()])
+    }
+
     pub fn get_block_transaction_count_by_number(&mut self, number: &str) -> String {
         self.rpc_request::<String>("eth_getBlockTransactionCountByNumber",
                                    vec![number.to_string()])
+    }
+
+    pub fn get_uncle_count_by_block_hash(&mut self, hash: &str) -> String {
+        self.rpc_request::<String>("eth_getUncleCountByBlockHash",
+                                   vec![hash.to_string()])
+    }
+
+    pub fn get_uncle_count_by_block_number(&mut self, number: &str) -> String {
+        self.rpc_request::<String>("eth_getUncleCountByBlockNumber",
+                                   vec![number.to_string()])
+    }
+
+    pub fn get_code(&mut self, address: &str, number: &str) -> String {
+        self.rpc_request::<String>("eth_getCode", vec![address.to_string(), number.to_string()])
+    }
+
+    pub fn sign(&mut self, address: &str, message: &str) -> String {
+        self.rpc_request::<String>("eth_sign", vec![address.to_string(), message.to_string()])
+    }
+
+    pub fn send_transaction(&mut self, transaction: RPCTransaction) -> String {
+        unimplemented!()
+    }
+
+    pub fn send_raw_transaction(&mut self, data: &str) -> String {
+        self.rpc_request::<String>("eth_sendRawTransaction", vec![data.to_string()])
+    }
+
+    pub fn call(&mut self, transaction: RPCTransaction) -> String {
+        unimplemented!()
+    }
+
+    pub fn estimate_gas(&mut self, transaction: RPCTransaction) -> String {
+        unimplemented!()
+    }
+
+    pub fn get_block_by_hash(&mut self, hash: &str) -> RPCBlock {
+        self.rpc_request::<RPCBlock>("eth_getBlockByHash",
+                                     vec![hash.to_string(), "false".to_string()])
+    }
+
+    pub fn get_block_by_number(&mut self, number: &str) -> RPCBlock {
+        self.rpc_request::<RPCBlock>("eth_getBlockByNumber",
+                                     vec![number.to_string(), "false".to_string()])
+    }
+
+    pub fn get_transaction_by_hash(&mut self, hash: &str) -> RPCTransaction {
+        self.rpc_request::<RPCTransaction>("eth_getTransactionByHash",
+                                           vec![hash.to_string()])
+    }
+
+    pub fn get_transaction_by_block_hash_and_index(&mut self, hash: &str, index: &str) -> RPCTransaction {
+        self.rpc_request::<RPCTransaction>("eth_getTransactionByBlockHashAndIndex",
+                                           vec![hash.to_string(), index.to_string()])
     }
 
     pub fn get_transaction_by_block_number_and_index(&mut self, number: &str, index: &str)
@@ -108,17 +265,97 @@ impl GethRPCClient {
                                            vec![number.to_string(), index.to_string()])
     }
 
-    pub fn get_block_by_number(&mut self, number: &str) -> RPCBlock {
-        self.rpc_request::<RPCBlock>("eth_getBlockByNumber",
-                                     vec![number.to_string(), "false".to_string()])
+    pub fn get_transaction_receipt(&mut self, hash: &str) -> RPCTransactionReceipt {
+        self.rpc_request::<RPCTransactionReceipt>("eth_getTransactionReceipt",
+                                                 vec![hash.to_string()])
     }
 
-    pub fn block_number(&mut self) -> String {
-        self.rpc_request::<String>("eth_blockNumber", vec![])
+    pub fn get_uncle_by_block_hash_and_index(&mut self, hash: &str, index: &str) -> RPCBlock {
+        self.rpc_request::<RPCBlock>("eth_getUncleByBlockHashAndIndex",
+                                     vec![hash.to_string(), index.to_string()])
     }
 
-    pub fn get_code(&mut self, address: &str, number: &str) -> String {
-        self.rpc_request::<String>("eth_getCode", vec![address.to_string(), number.to_string()])
+    pub fn get_uncle_by_block_number_and_index(&mut self, number: &str, index: &str) -> RPCBlock {
+        self.rpc_request::<RPCBlock>("eth_getUncleByBlockNumberAndIndex",
+                                     vec![number.to_string(), index.to_string()])
+    }
+
+    pub fn get_compilers(&mut self) -> Vec<String> {
+        self.rpc_request::<Vec<String>>("eth_getCompilers", vec![])
+    }
+
+    pub fn compile_solidity(&mut self, source: &str) -> String {
+        unimplemented!()
+    }
+
+    pub fn compile_lll(&mut self, source: &str) -> String {
+        self.rpc_request::<String>("eth_compileLLL", vec![source.to_string()])
+    }
+
+    pub fn compile_serpent(&mut self, source: &str) -> String {
+        self.rpc_request::<String>("eth_compileSerpent", vec![source.to_string()])
+    }
+
+    pub fn new_filter(&mut self, filter: RPCFilter) -> String {
+        unimplemented!()
+    }
+
+    pub fn new_block_filter(&mut self) -> String {
+        self.rpc_request::<String>("eth_newBlockFilter", vec![])
+    }
+
+    pub fn new_pending_transaction_filter(&mut self) -> String {
+        self.rpc_request::<String>("eth_newPendingTransactionFilter", vec![])
+    }
+
+    pub fn uninstall_filter(&mut self, id: &str) -> bool {
+        self.rpc_request::<bool>("eth_uninstallFilter", vec![id.to_string()])
+    }
+
+    pub fn get_filter_changes(&mut self, id: &str) -> String {
+        unimplemented!()
+    }
+
+    pub fn get_filter_logs(&mut self, id: &str) -> String {
+        unimplemented!()
+    }
+
+    pub fn get_logs(&mut self, id: &str) -> String {
+        unimplemented!()
+    }
+
+    pub fn get_work(&mut self) -> Vec<String> {
+        self.rpc_request::<Vec<String>>("eth_getWork", vec![])
+    }
+
+    pub fn submit_work(&mut self, nonce: &str, pow: &str, mix: &str) -> bool {
+        self.rpc_request::<bool>("eth_submitWork",
+                                 vec![nonce.to_string(), pow.to_string(), mix.to_string()])
+    }
+
+    pub fn submit_hashrate(&mut self, hashrate: &str, id: &str) -> bool {
+        self.rpc_request::<bool>("eth_submitHashrate",
+                                 vec![hashrate.to_string(), id.to_string()])
+    }
+
+    pub fn put_string(&mut self, db: &str, key: &str, value: &str) -> bool {
+        self.rpc_request::<bool>("db_putString",
+                                 vec![db.to_string(), key.to_string(), value.to_string()])
+    }
+
+    pub fn get_string(&mut self, db: &str, key: &str) -> String {
+        self.rpc_request::<String>("db_getString",
+                                   vec![db.to_string(), key.to_string()])
+    }
+
+    pub fn put_hex(&mut self, db: &str, key: &str, value: &str) -> bool {
+        self.rpc_request::<bool>("db_putHex",
+                                 vec![db.to_string(), key.to_string(), value.to_string()])
+    }
+
+    pub fn get_hex(&mut self, db: &str, key: &str) -> String {
+        self.rpc_request::<String>("db_getHex",
+                                   vec![db.to_string(), key.to_string()])
     }
 }
 
