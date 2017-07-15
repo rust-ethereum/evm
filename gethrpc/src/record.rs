4629e1e84cc81ae9f9fc69734884b8d8a8070de8
Elaine Ou <elaine.ou@gmail.com>
2017-07-14 20:43:46 -0700

    gethrpc/src/lib, record: add TLS-capable client for hyper

--- gethrpc/src/record.rs
+++ gethrpc/src/record.rs
@@ -5,7 +5,9 @@ use serde;
 use serde_json;
 use std::io::Read;
 use hyper::header::ContentType;
-use hyper::client::Client;
+use hyper::Client;
+use hyper::net::HttpsConnector;
+use hyper_native_tls::NativeTlsClient;
 
 #[derive(Serialize, Deserialize)]
 struct Record {
@@ -23,10 +25,12 @@ pub struct RecordGethRPCClient {
 
 impl RecordGethRPCClient {
     pub fn new(endpoint: &str) -> Self {
+        let ssl = NativeTlsClient::new().unwrap();
+        let connector = HttpsConnector::new(ssl);
         RecordGethRPCClient {
             endpoint: endpoint.to_string(),
             free_id: 1,
-            http: Client::new(),
+            http: Client::with_connector(connector),
             records: Vec::new(),
         }
     }
@@ -37,7 +41,11 @@ impl RecordGethRPCClient {
 }
 
 impl GethRPCClient for RecordGethRPCClient {
-    fn rpc_object_request<Req: serde::Serialize, Res: serde::Deserialize>(&mut self, method: &str, params: Req) -> Res {
+    fn rpc_object_request<Req: serde::Serialize, Res: serde::Deserialize>(
+        &mut self,
+        method: &str,
+        params: Req,
+    ) -> Res {
         let request_value = serde_json::to_value(params).unwrap();
         let request = RPCObjectRequest {
             jsonrpc: "2.0".to_string(),
@@ -47,15 +55,18 @@ impl GethRPCClient for RecordGethRPCClient {
         };
         self.free_id = self.free_id + 1;
 
-        let mut response_raw = self.http.post(&self.endpoint)
+        let mut response_raw = self.http
+            .post(&self.endpoint)
             .header(ContentType::json())
             .body(&serde_json::to_string(&request).unwrap())
-            .send().unwrap();
+            .send()
+            .unwrap();
         let mut buffer = String::new();
         response_raw.read_to_string(&mut buffer).unwrap();
 
         let response_value: serde_json::Value = serde_json::from_str(&buffer).unwrap();
-        let response: RPCObjectResponse<Res> = serde_json::from_value(response_value.clone()).unwrap();
+        let response: RPCObjectResponse<Res> = serde_json::from_value(response_value.clone())
+            .unwrap();
 
         self.records.push(Record {
             method: method.to_string(),
@@ -78,12 +89,17 @@ impl CachedGethRPCClient {
 }
 
 impl GethRPCClient for CachedGethRPCClient {
-    fn rpc_object_request<Req: serde::Serialize, Res: serde::Deserialize>(&mut self, method: &str, params: Req) -> Res {
+    fn rpc_object_request<Req: serde::Serialize, Res: serde::Deserialize>(
+        &mut self,
+        method: &str,
+        params: Req,
+    ) -> Res {
         let request_value = serde_json::to_value(params).unwrap();
 
         for record in &self.records {
             if &record.method == method && record.request == request_value {
-                let response: RPCObjectResponse<Res> = serde_json::from_value(record.response.clone()).unwrap();
+                let response: RPCObjectResponse<Res> =
+                    serde_json::from_value(record.response.clone()).unwrap();
                 return response.result;
             }
         }
