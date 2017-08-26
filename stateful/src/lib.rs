fc90c50f47a31c2fb5395d753515d07659efbbdc
Wei Tang <hi@that.world>
2017-08-27 00:06:44 +0800

    stateful: fix lifetime error and add genesis bloc test

--- stateful/src/lib.rs
+++ stateful/src/lib.rs
@@ -1,4 +1,4 @@
-#![deny(unused_import_braces, unused_imports,
+#![deny(unused_import_braces,
         unused_comparisons, unused_must_use,
         unused_variables, non_shorthand_field_patterns,
         unreachable_code)]
@@ -7,196 +7,233 @@ extern crate trie;
 extern crate sputnikvm;
 extern crate sha3;
 extern crate block;
+extern crate rlp;
 
-use sputnikvm::{H256, U256, M256};
-use sputnikvm::vm::{self, ValidTransaction, HeaderParams, Memory, TransactionVM, VM,
-                    AccountCommitment, Patch};
-use sputnikvm::vm::errors::RequireError;
-use trie::{MemoryDatabase, Database, DatabaseGuard};
-use block::Account;
-use sha3::{Digest, Keccak256};
-use std::collections::HashMap;
+use trie::MemoryDatabase;
 
-pub struct Stateful<D, G> {
-    database: D,
-    code_hashes: G,
-    root: H256,
-}
+macro_rules! stateful {
+    ($name: tt, $database: tt) => {
+        use sputnikvm::{H256, U256, M256};
+        use sputnikvm::vm::{self, ValidTransaction, HeaderParams, Memory, TransactionVM, VM,
+                            AccountCommitment, Patch, SeqMemory};
+        use sputnikvm::vm::errors::RequireError;
+        use trie::{Trie, DatabaseGuard};
+        use block::Account;
+        use sha3::{Digest, Keccak256};
+        use std::collections::HashMap;
+        use std::cmp::min;
 
-impl<D, G> Stateful<D, G> {
-    pub fn new(database: D, code_hashes: G, root: H256) -> Self {
-        Self {
-            database,
-            code_hashes,
-            root
+        pub struct $name<G> {
+            database: $database,
+            code_hashes: G,
+            root: H256,
         }
-    }
-}
 
-impl<D: Default, G: Default> Default for Stateful<D, G> {
-    fn default() -> Self {
-        Stateful {
-            database: D::default(),
-            code_hashes: G::default(),
-            root: MemoryDatabase::new().create_empty().root(),
-        }
-    }
-}
-
-fn is_empty_hash(hash: H256) -> bool {
-    hash == H256::from(Keccak256::digest(&[]).as_slice())
-}
-
-impl<'a, D: Database<'a>, G: DatabaseGuard> Stateful<D, G> {
-    pub fn call<M: Memory + Default>(
-        &'a self, transaction: ValidTransaction, block: HeaderParams,
-        patch: &'static Patch, most_recent_block_hashes: &[H256]
-    ) -> TransactionVM<M> {
-        let mut vm = TransactionVM::new(transaction, block.clone(), patch);
-        let state = self.database.create_trie(self.root);
-
-        loop {
-            match vm.fire() {
-                Ok(()) => break,
-                Err(RequireError::Account(address)) => {
-                    let account: Option<Account> = state.get(&address);
-
-                    match account {
-                        Some(account) => {
-                            let code = if is_empty_hash(account.code_hash) {
-                                Vec::new()
-                            } else {
-                                self.code_hashes.get(account.code_hash).unwrap()
-                            };
-
-                            vm.commit_account(AccountCommitment::Full {
-                                nonce: account.nonce,
-                                address: address,
-                                balance: account.balance,
-                                code: code,
-                            }).unwrap();
-                        },
-                        None => {
-                            vm.commit_account(AccountCommitment::Nonexist(address)).unwrap();
-                        },
-                    }
-                },
-                Err(RequireError::AccountCode(address)) => {
-                    let account: Option<Account> = state.get(&address);
-
-                    match account {
-                        Some(account) => {
-                            let code = if is_empty_hash(account.code_hash) {
-                                Vec::new()
-                            } else {
-                                self.code_hashes.get(account.code_hash).unwrap()
-                            };
-
-                            vm.commit_account(AccountCommitment::Code {
-                                address: address,
-                                code: code,
-                            }).unwrap();
-                        },
-                        None => {
-                            vm.commit_account(AccountCommitment::Nonexist(address)).unwrap();
-                        },
-                    }
-                },
-                Err(RequireError::AccountStorage(address, index)) => {
-                    let account: Option<Account> = state.get(&address);
-
-                    match account {
-                        Some(account) => {
-                            let storage = self.database.create_trie(account.storage_root);
-                            let value = storage.get(&index).unwrap_or(M256::zero());
-
-                            vm.commit_account(AccountCommitment::Storage {
-                                address: address,
-                                index, value
-                            }).unwrap();
-                        },
-                        None => {
-                            vm.commit_account(AccountCommitment::Nonexist(address)).unwrap();
-                        },
-                    }
-                },
-                Err(RequireError::Blockhash(number)) => {
-                    let index = (block.number - number).as_usize();
-                    vm.commit_blockhash(number, most_recent_block_hashes[index]).unwrap();
-                },
+        impl<G> $name<G> {
+            pub fn new(database: $database, code_hashes: G, root: H256) -> Self {
+                Self {
+                    database,
+                    code_hashes,
+                    root
+                }
             }
         }
 
-        vm
-    }
-
-    pub fn transit(
-        &'a mut self, accounts: &[vm::Account]
-    ) {
-        let mut state = self.database.create_trie(self.root);
-
-        for account in accounts {
-            match account.clone() {
-                vm::Account::Full {
-                    nonce, address, balance, changing_storage, code
-                } => {
-                    let changing_storage: HashMap<U256, M256> = changing_storage.into();
-
-                    let mut account: Account = state.get(&address).unwrap();
-
-                    let mut storage_trie = self.database.create_trie(account.storage_root);
-                    for (key, value) in changing_storage {
-                        storage_trie.insert(key, value);
-                    }
-
-                    account.balance = balance;
-                    account.nonce = nonce;
-                    account.storage_root = storage_trie.root();
-                    assert!(account.code_hash == H256::from(Keccak256::digest(&code).as_slice()));
-
-                    state.insert(address, account);
-                },
-                vm::Account::IncreaseBalance(address, value) => {
-                    let mut account: Account = state.get(&address).unwrap();
-
-                    account.balance = account.balance + value;
-                    state.insert(address, account);
-                },
-                vm::Account::DecreaseBalance(address, value) => {
-                    let mut account: Account = state.get(&address).unwrap();
-
-                    account.balance = account.balance - value;
-                    state.insert(address, account);
-                },
-                vm::Account::Create {
-                    nonce, address, balance, storage, code, exists
-                } => {
-                    if !exists {
-                        state.remove(&address);
-                    } else {
-                        let storage: HashMap<U256, M256> = storage.into();
-
-                        let mut storage_trie = self.database.create_empty();
-                        for (key, value) in storage {
-                            storage_trie.insert(key, value);
-                        }
-
-                        let code_hash = H256::from(Keccak256::digest(&code).as_slice());
-                        self.code_hashes.set(code_hash, code);
-
-                        let account = Account {
-                            nonce: nonce,
-                            balance: balance,
-                            storage_root: storage_trie.root(),
-                            code_hash
-                        };
-
-                        state.insert(address, account);
-                    }
-                },
+        impl<G: Default> Default for $name<G> {
+            fn default() -> Self {
+                Self {
+                    database: $database::default(),
+                    code_hashes: G::default(),
+                    root: MemoryDatabase::new().create_empty().root(),
+                }
             }
         }
 
-        self.root = state.root();
+        impl<G: DatabaseGuard> $name<G> {
+            fn is_empty_hash(hash: H256) -> bool {
+                hash == H256::from(Keccak256::digest(&[]).as_slice())
+            }
+
+            pub fn call<M: Memory + Default>(
+                &self, transaction: ValidTransaction, block: HeaderParams,
+                patch: &'static Patch, most_recent_block_hashes: &[H256]
+            ) -> TransactionVM<M> {
+                assert!(U256::from(most_recent_block_hashes.len()) >=
+                        min(block.number, U256::from(256)));
+
+                let mut vm = TransactionVM::new(transaction, block.clone(), patch);
+                let state = self.database.create_trie(self.root);
+
+                loop {
+                    match vm.fire() {
+                        Ok(()) => break,
+                        Err(RequireError::Account(address)) => {
+                            let account: Option<Account> = state.get(&address);
+
+                            match account {
+                                Some(account) => {
+                                    let code = if Self::is_empty_hash(account.code_hash) {
+                                        Vec::new()
+                                    } else {
+                                        self.code_hashes.get(account.code_hash).unwrap()
+                                    };
+
+                                    vm.commit_account(AccountCommitment::Full {
+                                        nonce: account.nonce,
+                                        address: address,
+                                        balance: account.balance,
+                                        code: code,
+                                    }).unwrap();
+                                },
+                                None => {
+                                    vm.commit_account(AccountCommitment::Nonexist(address)).unwrap();
+                                },
+                            }
+                        },
+                        Err(RequireError::AccountCode(address)) => {
+                            let account: Option<Account> = state.get(&address);
+
+                            match account {
+                                Some(account) => {
+                                    let code = if Self::is_empty_hash(account.code_hash) {
+                                        Vec::new()
+                                    } else {
+                                        self.code_hashes.get(account.code_hash).unwrap()
+                                    };
+
+                                    vm.commit_account(AccountCommitment::Code {
+                                        address: address,
+                                        code: code,
+                                    }).unwrap();
+                                },
+                                None => {
+                                    vm.commit_account(AccountCommitment::Nonexist(address)).unwrap();
+                                },
+                            }
+                        },
+                        Err(RequireError::AccountStorage(address, index)) => {
+                            let account: Option<Account> = state.get(&address);
+
+                            match account {
+                                Some(account) => {
+                                    let storage = self.database.create_trie(account.storage_root);
+                                    let value = storage.get(&index).unwrap_or(M256::zero());
+
+                                    vm.commit_account(AccountCommitment::Storage {
+                                        address: address,
+                                        index, value
+                                    }).unwrap();
+                                },
+                                None => {
+                                    vm.commit_account(AccountCommitment::Nonexist(address)).unwrap();
+                                },
+                            }
+                        },
+                        Err(RequireError::Blockhash(number)) => {
+                            let index = (block.number - number).as_usize();
+                            vm.commit_blockhash(number, most_recent_block_hashes[index]).unwrap();
+                        },
+                    }
+                }
+
+                vm
+            }
+
+            pub fn transit(
+                &mut self, accounts: &[vm::Account]
+            ) {
+                let mut state = self.database.create_trie(self.root);
+
+                for account in accounts {
+                    match account.clone() {
+                        vm::Account::Full {
+                            nonce, address, balance, changing_storage, code
+                        } => {
+                            let changing_storage: HashMap<U256, M256> = changing_storage.into();
+
+                            let mut account: Account = state.get(&address).unwrap();
+
+                            let mut storage_trie = self.database.create_trie(account.storage_root);
+                            for (key, value) in changing_storage {
+                                storage_trie.insert(key, value);
+                            }
+
+                            account.balance = balance;
+                            account.nonce = nonce;
+                            account.storage_root = storage_trie.root();
+                            assert!(account.code_hash == H256::from(Keccak256::digest(&code).as_slice()));
+
+                            state.insert_raw(Keccak256::digest(&address).as_slice().into(),
+                                             rlp::encode(&account).to_vec());
+                        },
+                        vm::Account::IncreaseBalance(address, value) => {
+                            let mut account: Account = state.get(&address).unwrap();
+
+                            account.balance = account.balance + value;
+
+                            state.insert_raw(Keccak256::digest(&address).as_slice().into(),
+                                             rlp::encode(&account).to_vec());
+                        },
+                        vm::Account::DecreaseBalance(address, value) => {
+                            let mut account: Account = state.get(&address).unwrap();
+
+                            account.balance = account.balance - value;
+
+                            state.insert_raw(Keccak256::digest(&address).as_slice().into(),
+                                             rlp::encode(&account).to_vec());
+                        },
+                        vm::Account::Create {
+                            nonce, address, balance, storage, code, exists
+                        } => {
+                            if !exists {
+                                state.remove_raw(Keccak256::digest(&address).as_slice());
+                            } else {
+                                let storage: HashMap<U256, M256> = storage.into();
+
+                                let mut storage_trie = self.database.create_empty();
+                                for (key, value) in storage {
+                                    storage_trie.insert(key, value);
+                                }
+
+                                let code_hash = H256::from(Keccak256::digest(&code).as_slice());
+                                self.code_hashes.set(code_hash, code);
+
+                                let account = Account {
+                                    nonce: nonce,
+                                    balance: balance,
+                                    storage_root: storage_trie.root(),
+                                    code_hash
+                                };
+
+                                state.insert_raw(Keccak256::digest(&address).as_slice().into(),
+                                                 rlp::encode(&account).to_vec());
+                            }
+                        },
+                    }
+                }
+
+                self.root = state.root();
+            }
+
+            pub fn execute<M: Memory + Default>(
+                &mut self, transaction: ValidTransaction, block: HeaderParams,
+                patch: &'static Patch, most_recent_block_hashes: &[H256]
+            ) -> TransactionVM<M> {
+                let vm = self.call(transaction, block, patch, most_recent_block_hashes);
+                let mut accounts = Vec::new();
+                for account in vm.accounts() {
+                    accounts.push(account.clone());
+                }
+                self.transit(&accounts);
+                vm
+            }
+
+            pub fn root(&self) -> H256 {
+                self.root
+            }
+        }
     }
 }
+
+stateful!(GenericMemoryStateful, MemoryDatabase);
+pub type MemoryStateful = GenericMemoryStateful<HashMap<H256, Vec<u8>>>;
---
+++ stateful/tests/genesis.rs
@@ -0,0 +1,115 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate lazy_static;
+extern crate sputnikvm;
+extern crate sputnikvm_stateful;
+extern crate block;
+extern crate trie;
+extern crate rand;
+extern crate sha3;
+
+use sha3::{Digest, Keccak256};
+use sputnikvm::{H256, U256, Address, Gas};
+use sputnikvm::vm::{self, ValidTransaction, Storage, VM, SeqTransactionVM, HeaderParams, EIP160_PATCH, VMStatus};
+use sputnikvm_stateful::MemoryStateful;
+use block::TransactionAction;
+use trie::MemoryDatabase;
+use std::collections::HashMap;
+use std::str::FromStr;
+use rand::Rng;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct JSONAccount {
+    balance: String,
+}
+
+lazy_static! {
+    static ref GENESIS_ACCOUNTS: HashMap<String, JSONAccount> =
+        serde_json::from_str(include_str!("../res/genesis.json")).unwrap();
+}
+
+lazy_static! {
+    static ref MORDEN_ACCOUNTS: HashMap<String, JSONAccount> =
+        serde_json::from_str(include_str!("../res/morden.json")).unwrap();
+}
+
+#[test]
+fn secure_trie() {
+    let mut database = MemoryDatabase::new();
+    let mut trie = database.create_empty();
+
+    trie.insert_raw(Keccak256::digest("doe".as_bytes()).as_slice().into(),
+                    "reindeer".as_bytes().into());
+    trie.insert_raw(Keccak256::digest("dog".as_bytes()).as_slice().into(),
+                    "puppy".as_bytes().into());
+    trie.insert_raw(Keccak256::digest("dogglesworth".as_bytes()).as_slice().into(),
+                    "cat".as_bytes().into());
+
+    assert_eq!(trie.root(), H256::from_str("0xd4cd937e4a4368d7931a9cf51686b7e10abb3dce38a39000fd7902a092b64585").unwrap());
+}
+
+#[test]
+fn morden_state_root() {
+    let mut stateful = MemoryStateful::default();
+    let mut rng = rand::thread_rng();
+
+    let mut accounts: Vec<(&String, &JSONAccount)> = MORDEN_ACCOUNTS.iter().collect();
+    rng.shuffle(&mut accounts);
+
+    for (key, value) in accounts {
+        let address = Address::from_str(key).unwrap();
+        let balance = U256::from_dec_str(&value.balance).unwrap();
+
+        stateful.transit(
+            &[vm::Account::Create {
+                address,
+                balance,
+                storage: Storage::new(address, false),
+                code: Vec::new(),
+                nonce: U256::from(2u64.pow(20)),
+                exists: true,
+            }]
+        );
+    }
+
+    assert_eq!(stateful.root(), H256::from("0xf3f4696bbf3b3b07775128eb7a3763279a394e382130f27c21e70233e04946a9"));
+}
+
+#[test]
+fn genesis_state_root() {
+    let mut stateful = MemoryStateful::default();
+    let mut rng = rand::thread_rng();
+
+    let mut accounts: Vec<(&String, &JSONAccount)> = GENESIS_ACCOUNTS.iter().collect();
+    rng.shuffle(&mut accounts);
+
+    for (key, value) in accounts {
+        let address = Address::from_str(key).unwrap();
+        let balance = U256::from_dec_str(&value.balance).unwrap();
+
+        let vm: SeqTransactionVM = stateful.execute(ValidTransaction {
+            caller: None,
+            gas_price: Gas::zero(),
+            gas_limit: Gas::from(100000u64),
+            action: TransactionAction::Call(address),
+            value: balance,
+            input: Vec::new(),
+            nonce: U256::zero(),
+        }, HeaderParams {
+            beneficiary: Address::default(),
+            timestamp: 0,
+            number: U256::zero(),
+            difficulty: U256::zero(),
+            gas_limit: Gas::max_value()
+        }, &EIP160_PATCH, &[]);
+        match vm.status() {
+            VMStatus::ExitedOk => (),
+            _ => panic!(),
+        }
+    }
+
+    assert_eq!(stateful.root(), H256::from("0xd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544"));
+}
