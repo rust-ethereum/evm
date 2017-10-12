f55fcae81eb2ee9ccdd91d548072d62f55ebda3c
Wei Tang <hi@that.world>
2017-10-12 23:48:58 +0800

    regtests: fix patch name changes

--- regtests/src/bin/main.rs
+++ regtests/src/bin/main.rs
@@ -319,7 +319,7 @@ fn test_all_previously_failed_frontier_blocks() {
     let numbers: Vec<usize> = serde_json::from_str(include_str!("../../res/frontier_numbers.json")).unwrap();
     let mut client = CachedGethRPCClient::from_value(cached);
     for n in numbers {
-        test_block::<_, FrontierPatch>(&mut client, n);
+        test_block::<_, MainnetFrontierPatch>(&mut client, n);
     }
 }
 
@@ -330,7 +330,7 @@ fn test_all_previously_failed_homestead_blocks() {
     let numbers: Vec<usize> = serde_json::from_str(include_str!("../../res/homestead_numbers.json")).unwrap();
     let mut client = CachedGethRPCClient::from_value(cached);
     for n in numbers {
-        test_block::<_, HomesteadPatch>(&mut client, n);
+        test_block::<_, MainnetHomesteadPatch>(&mut client, n);
     }
 }
 
@@ -341,6 +341,6 @@ fn test_all_previously_failed_eip150_blocks() {
     let numbers: Vec<usize> = serde_json::from_str(include_str!("../../res/eip150_numbers.json")).unwrap();
     let mut client = CachedGethRPCClient::from_value(cached);
     for n in numbers {
-        test_block::<_, EIP150Patch>(&mut client, n);
+        test_block::<_, MainnetEIP150Patch>(&mut client, n);
     }
 }
