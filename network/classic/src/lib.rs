pouladzade/sputnikvm

byzantium patch

network/classic/src/lib.rs
@@ -1,10 +1,14 @@
 extern crate bigint;
 extern crate sputnikvm;
+extern crate sputnikvm_precompiled_modexp;
+extern crate sputnikvm_precompiled_bn128;
 
 use std::marker::PhantomData;
 use bigint::{Gas, U256, H160, Address};
 use sputnikvm::{Precompiled, AccountPatch, Patch,
                 ID_PRECOMPILED, ECREC_PRECOMPILED, SHA256_PRECOMPILED, RIP160_PRECOMPILED};
+use sputnikvm_precompiled_modexp::MODEXP_PRECOMPILED;
+use sputnikvm_precompiled_bn128::{BN128_ADD_PRECOMPILED, BN128_MUL_PRECOMPILED, BN128_PAIRING_PRECOMPILED};
 
 /// Mainnet account patch
 pub struct MainnetAccountPatch;
@@ -34,6 +38,33 @@ pub static ETC_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompi
     (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
      None,
      &ID_PRECOMPILED),
+];
+
+pub static BYZANTIUM_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompiled); 8] = [
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x01]),
+     None,
+     &ECREC_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x02]),
+     None,
+     &SHA256_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x03]),
+     None,
+     &RIP160_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
+     None,
+     &ID_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x05]),
+     None,
+     &MODEXP_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x06]),
+     None,
+     &BN128_ADD_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x07]),
+     None,
+     &BN128_MUL_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x08]),
+     None,
+     &BN128_PAIRING_PRECOMPILED),
 ];
 
 /// Frontier patch.
@@ -150,4 +181,33 @@ impl<A: AccountPatch> Patch for EIP160Patch<A> {
     fn memory_limit() -> usize { usize::max_value() }
     fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
         &ETC_PRECOMPILEDS }
+}
+
+/// Byzantium patch.
+pub struct ByzantiumPatch<A: AccountPatch>(PhantomData<A>);
+pub type MainnetByzantiumPatch = ByzantiumPatch<MainnetAccountPatch>;
+pub type MordenByzantiumPatch = ByzantiumPatch<MordenAccountPatch>;
+impl<A: AccountPatch> Patch for ByzantiumPatch<A> {
+    type Account = A;
+
+    fn code_deposit_limit() -> Option<usize> { None }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(700usize) }
+    fn gas_balance() -> Gas { Gas::from(400usize) }
+    fn gas_sload() -> Gas { Gas::from(200usize) }
+    fn gas_suicide() -> Gas { Gas::from(5000usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(25000usize) }
+    fn gas_call() -> Gas { Gas::from(700usize) }
+    fn gas_expbyte() -> Gas { Gas::from(50usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(32000usize) }
+    fn force_code_deposit() -> bool { false }
+    fn has_delegate_call() -> bool { true }
+    fn has_static_call() -> bool { true }
+    fn has_revert() -> bool { true }
+    fn has_return_data() -> bool { true }
+    fn err_on_call_with_more_gas() -> bool { false }
+    fn call_create_l64_after_gas() -> bool { true }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &BYZANTIUM_PRECOMPILEDS }
 }

rust-ethereum/evm

network/classic/src/lib.rs
@@ -1,10 +1,14 @@
 extern crate bigint;
 extern crate evm;
+extern crate evm_precompiled_modexp;
+extern crate evm_precompiled_bn128;
 
 use std::marker::PhantomData;
 use bigint::{Gas, U256, H160, Address};
 use evm::{Precompiled, AccountPatch, Patch,
           ID_PRECOMPILED, ECREC_PRECOMPILED, SHA256_PRECOMPILED, RIP160_PRECOMPILED};
+use evm_precompiled_modexp::MODEXP_PRECOMPILED;
+use evm_precompiled_bn128::{BN128_ADD_PRECOMPILED, BN128_MUL_PRECOMPILED, BN128_PAIRING_PRECOMPILED};
 
 /// Mainnet account patch
 pub struct MainnetAccountPatch;
@@ -34,6 +38,33 @@ pub static ETC_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompi
     (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
      None,
      &ID_PRECOMPILED),
+];
+
+pub static BYZANTIUM_PRECOMPILEDS: [(Address, Option<&'static [u8]>, &'static Precompiled); 8] = [
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x01]),
+     None,
+     &ECREC_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x02]),
+     None,
+     &SHA256_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x03]),
+     None,
+     &RIP160_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x04]),
+     None,
+     &ID_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x05]),
+     None,
+     &MODEXP_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x06]),
+     None,
+     &BN128_ADD_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x07]),
+     None,
+     &BN128_MUL_PRECOMPILED),
+    (H160([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x08]),
+     None,
+     &BN128_PAIRING_PRECOMPILED),
 ];
 
 /// Frontier patch.
@@ -150,4 +181,33 @@ impl<A: AccountPatch> Patch for EIP160Patch<A> {
     fn memory_limit() -> usize { usize::max_value() }
     fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
         &ETC_PRECOMPILEDS }
+}
+
+/// Byzantium patch.
+pub struct ByzantiumPatch<A: AccountPatch>(PhantomData<A>);
+pub type MainnetByzantiumPatch = ByzantiumPatch<MainnetAccountPatch>;
+pub type MordenByzantiumPatch = ByzantiumPatch<MordenAccountPatch>;
+impl<A: AccountPatch> Patch for ByzantiumPatch<A> {
+    type Account = A;
+
+    fn code_deposit_limit() -> Option<usize> { None }
+    fn callstack_limit() -> usize { 1024 }
+    fn gas_extcode() -> Gas { Gas::from(700usize) }
+    fn gas_balance() -> Gas { Gas::from(400usize) }
+    fn gas_sload() -> Gas { Gas::from(200usize) }
+    fn gas_suicide() -> Gas { Gas::from(5000usize) }
+    fn gas_suicide_new_account() -> Gas { Gas::from(25000usize) }
+    fn gas_call() -> Gas { Gas::from(700usize) }
+    fn gas_expbyte() -> Gas { Gas::from(50usize) }
+    fn gas_transaction_create() -> Gas { Gas::from(32000usize) }
+    fn force_code_deposit() -> bool { false }
+    fn has_delegate_call() -> bool { true }
+    fn has_static_call() -> bool { true }
+    fn has_revert() -> bool { true }
+    fn has_return_data() -> bool { true }
+    fn err_on_call_with_more_gas() -> bool { false }
+    fn call_create_l64_after_gas() -> bool { true }
+    fn memory_limit() -> usize { usize::max_value() }
+    fn precompileds() -> &'static [(Address, Option<&'static [u8]>, &'static Precompiled)] {
+        &BYZANTIUM_PRECOMPILEDS }
 }