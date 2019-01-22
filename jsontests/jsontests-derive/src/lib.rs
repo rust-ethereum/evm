ETCDEVTeam/sputnikvm

settable Patch in the testgen

jsontests/jsontests-derive/src/lib.rs
@@ -21,7 +21,7 @@ use itertools::Itertools;
 use syn::Ident;
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(JsonTests, attributes(directory, test_with, bench_with, criterion_config, skip, should_panic))]
+#[proc_macro_derive(JsonTests, attributes(directory, test_with, bench_with, criterion_config, skip, should_panic, patch))]
 pub fn json_tests(input: TokenStream) -> TokenStream {
     // Construct a string representation of the type definition
     let s = input.to_string();
@@ -110,6 +110,7 @@ fn generate_test(config: &Config, test_name: &Ident, data: &str, tokens: &mut qu
     let test_func_path = &config.test_with.path;
     let test_func_name = &config.test_with.name;
     let test_name_str = test_name.as_ref();
+    let (patch_name, patch_path) = derive_patch(config);
 
     tokens.append(quote!{#[test]});
     if config.should_panic {
@@ -119,8 +120,9 @@ fn generate_test(config: &Config, test_name: &Ident, data: &str, tokens: &mut qu
     tokens.append(quote! {
         fn #test_name() {
             use #test_func_path;
+            use #patch_path;
             let data = #data;
-            #test_func_name(#test_name_str, data);
+            #test_func_name::<#patch_name>(#test_name_str, data);
         }
     });
 }
@@ -137,11 +139,14 @@ fn generate_bench(config: &Config, test_name: &Ident, data: &str, tokens: &mut q
     let bench_name = format!("bench_{}", test_name.as_ref());
     let bench_ident = Ident::from(bench_name.as_ref());
 
+    let (patch_name, patch_path) = derive_patch(config);
+
     tokens.append(quote! {
         pub fn #bench_ident(c: &mut Criterion) {
             use #bench_func_path;
+            use #patch_path;
             let data = #data;
-            #bench_func_name(c, #bench_name, data);
+            #bench_func_name::<#patch_name>(c, #bench_name, data);
         }
     });
 
@@ -164,5 +169,16 @@ fn generate_criterion_macros(config: &Config, benches: &[Ident], tokens: &mut qu
             };
         };
         tokens.append(template.as_ref().replace("TARGETS", &benches));
+    }
+}
+
+fn derive_patch(config: &Config) -> (Ident, Ident) {
+    if let Some(patch) = config.patch.as_ref() {
+        (patch.name.clone(), patch.path.clone())
+    } else {
+        (
+            Ident::from("VMTestPatch"),
+            Ident::from("sputnikvm::VMTestPatch")
+        )
     }
 }

jsontests/src/lib.rs
@@ -19,9 +19,9 @@ use std::sync::{Arc, Mutex};
 use bigint::{Gas, M256, U256, H256, Address};
 use hexutil::*;
 use sputnikvm::errors::RequireError;
-use sputnikvm::{VM, SeqContextVM, Context, VMStatus, VMTestPatch};
+use sputnikvm::{VM, SeqContextVM, Context, VMStatus, Patch};
 
-pub fn fire_with_block(machine: &mut SeqContextVM<VMTestPatch>, block: &JSONBlock) {
+pub fn fire_with_block<P: Patch>(machine: &mut SeqContextVM<P>, block: &JSONBlock) {
     loop {
         match machine.fire() {
             Err(RequireError::Account(address)) => {
@@ -60,7 +60,7 @@ pub fn fire_with_block(machine: &mut SeqContextVM<VMTestPatch>, block: &JSONBloc
     }
 }
 
-pub fn apply_to_block(machine: &SeqContextVM<VMTestPatch>, block: &mut JSONBlock) {
+pub fn apply_to_block<P: Patch>(machine: &SeqContextVM<P>, block: &mut JSONBlock) {
     for account in machine.accounts() {
         let account = (*account).clone();
         block.apply_account(account);
@@ -71,16 +71,16 @@ pub fn apply_to_block(machine: &SeqContextVM<VMTestPatch>, block: &mut JSONBlock
     }
 }
 
-pub fn create_machine(v: &Value, block: &JSONBlock) -> SeqContextVM<VMTestPatch> {
+pub fn create_machine<P: Patch>(v: &Value, block: &JSONBlock) -> SeqContextVM<P> {
     let transaction = create_context(v);
 
-    SeqContextVM::<VMTestPatch>::new(transaction, block.block_header())
+    SeqContextVM::<P>::new(transaction, block.block_header())
 }
 
 // TODO: consider refactoring
 #[cfg_attr(feature = "cargo-clippy", allow(cyclomatic_complexity))]
 #[cfg_attr(feature = "cargo-clippy", allow(collapsible_if))]
-pub fn test_machine(v: &Value, machine: &SeqContextVM<VMTestPatch>, block: &JSONBlock, history: &[Context], debug: bool) -> bool {
+pub fn test_machine<P: Patch>(v: &Value, machine: &SeqContextVM<P>, block: &JSONBlock, history: &[Context], debug: bool) -> bool {
     let callcreates = &v["callcreates"];
 
     if callcreates.as_array().is_some() {
@@ -261,13 +261,13 @@ fn is_ok(status: &VMStatus) -> bool {
     }
 }
 
-pub fn test_transaction(_name: &str, v: &Value, debug: bool) -> Result<bool, VMStatus> {
+pub fn test_transaction<P: Patch>(_name: &str, v: &Value, debug: bool) -> Result<bool, VMStatus> {
     let _ = env_logger::try_init();
 
     let mut block = create_block(v);
     let history: Arc<Mutex<Vec<Context>>> = Arc::new(Mutex::new(Vec::new()));
     let history_closure = history.clone();
-    let mut machine = create_machine(v, &block);
+    let mut machine = create_machine::<P>(v, &block);
     machine.add_context_history_hook(move |context| {
         history_closure.lock().unwrap().push(context.clone());
     });
@@ -298,13 +298,13 @@ pub fn test_transaction(_name: &str, v: &Value, debug: bool) -> Result<bool, VMS
 
 use criterion::Criterion;
 
-pub fn bench_transaction(_name: &str, v: Value, c: &mut Criterion) {
+pub fn bench_transaction<P: Patch>(_name: &str, v: Value, c: &mut Criterion) {
     c.bench_function(_name, move |b| {
         b.iter_with_large_setup(|| {
             let block = create_block(&v);
             let history: Arc<Mutex<Vec<Context>>> = Arc::new(Mutex::new(Vec::new()));
             let history_closure = history.clone();
-            let mut machine = create_machine(&v, &block);
+            let mut machine = create_machine::<P>(&v, &block);
             machine.add_context_history_hook(move |context| {
                 history_closure.lock().unwrap().push(context.clone());
             });

jsontests/src/util.rs
@@ -2,15 +2,16 @@ use serde_json::Value;
 use serde_json as json;
 use test_transaction;
 use bench_transaction;
+use sputnikvm::Patch;
 
-pub fn run_test(name: &str, test: &str) {
+pub fn run_test<P: Patch>(name: &str, test: &str) {
     let test: Value = json::from_str(test).unwrap();
-    assert_eq!(test_transaction(name, &test, true), Ok(true));
+    assert_eq!(test_transaction::<P>(name, &test, true), Ok(true));
 }
 
 use criterion::Criterion;
 
-pub fn run_bench(c: &mut Criterion, name: &'static str, test: &str) {
+pub fn run_bench<P: Patch>(c: &mut Criterion, name: &'static str, test: &str) {
     let test: Value = json::from_str(test).unwrap();
-    bench_transaction(name, test, c);
+    bench_transaction::<P>(name, test, c);
 }

rust-ethereum/evm

jsontests/jsontests-derive/src/lib.rs
@@ -21,7 +21,7 @@ use itertools::Itertools;
 use syn::Ident;
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(JsonTests, attributes(directory, test_with, bench_with, criterion_config, skip, should_panic))]
+#[proc_macro_derive(JsonTests, attributes(directory, test_with, bench_with, criterion_config, skip, should_panic, patch))]
 pub fn json_tests(input: TokenStream) -> TokenStream {
     // Construct a string representation of the type definition
     let s = input.to_string();
@@ -110,6 +110,7 @@ fn generate_test(config: &Config, test_name: &Ident, data: &str, tokens: &mut qu
     let test_func_path = &config.test_with.path;
     let test_func_name = &config.test_with.name;
     let test_name_str = test_name.as_ref();
+    let (patch_name, patch_path) = derive_patch(config);
 
     tokens.append(quote!{#[test]});
     if config.should_panic {
@@ -119,8 +120,9 @@ fn generate_test(config: &Config, test_name: &Ident, data: &str, tokens: &mut qu
     tokens.append(quote! {
         fn #test_name() {
             use #test_func_path;
+            use #patch_path;
             let data = #data;
-            #test_func_name(#test_name_str, data);
+            #test_func_name::<#patch_name>(#test_name_str, data);
         }
     });
 }
@@ -137,11 +139,14 @@ fn generate_bench(config: &Config, test_name: &Ident, data: &str, tokens: &mut q
     let bench_name = format!("bench_{}", test_name.as_ref());
     let bench_ident = Ident::from(bench_name.as_ref());
 
+    let (patch_name, patch_path) = derive_patch(config);
+
     tokens.append(quote! {
         pub fn #bench_ident(c: &mut Criterion) {
             use #bench_func_path;
+            use #patch_path;
             let data = #data;
-            #bench_func_name(c, #bench_name, data);
+            #bench_func_name::<#patch_name>(c, #bench_name, data);
         }
     });
 
@@ -164,5 +169,16 @@ fn generate_criterion_macros(config: &Config, benches: &[Ident], tokens: &mut qu
             };
         };
         tokens.append(template.as_ref().replace("TARGETS", &benches));
+    }
+}
+
+fn derive_patch(config: &Config) -> (Ident, Ident) {
+    if let Some(patch) = config.patch.as_ref() {
+        (patch.name.clone(), patch.path.clone())
+    } else {
+        (
+            Ident::from("VMTestPatch"),
+            Ident::from("evm::VMTestPatch")
+        )
     }
 }

jsontests/src/lib.rs
@@ -20,9 +20,9 @@ use std::sync::{Arc, Mutex};
 use bigint::{Gas, M256, U256, H256, Address};
 use hexutil::*;
 use evm::errors::RequireError;
-use evm::{VM, SeqContextVM, Context, VMStatus, VMTestPatch};
+use evm::{VM, SeqContextVM, Context, VMStatus, Patch};
 
-pub fn fire_with_block(machine: &mut SeqContextVM<VMTestPatch>, block: &JSONBlock) {
+pub fn fire_with_block<P: Patch>(machine: &mut SeqContextVM<P>, block: &JSONBlock) {
     loop {
         match machine.fire() {
             Err(RequireError::Account(address)) => {
@@ -61,7 +61,7 @@ pub fn fire_with_block(machine: &mut SeqContextVM<VMTestPatch>, block: &JSONBloc
     }
 }
 
-pub fn apply_to_block(machine: &SeqContextVM<VMTestPatch>, block: &mut JSONBlock) {
+pub fn apply_to_block<P: Patch>(machine: &SeqContextVM<P>, block: &mut JSONBlock) {
     for account in machine.accounts() {
         let account = (*account).clone();
         block.apply_account(account);
@@ -72,16 +72,16 @@ pub fn apply_to_block(machine: &SeqContextVM<VMTestPatch>, block: &mut JSONBlock
     }
 }
 
-pub fn create_machine(v: &Value, block: &JSONBlock) -> SeqContextVM<VMTestPatch> {
+pub fn create_machine<P: Patch>(v: &Value, block: &JSONBlock) -> SeqContextVM<P> {
     let transaction = create_context(v);
 
-    SeqContextVM::<VMTestPatch>::new(transaction, block.block_header())
+    SeqContextVM::<P>::new(transaction, block.block_header())
 }
 
 // TODO: consider refactoring
 #[cfg_attr(feature = "cargo-clippy", allow(cyclomatic_complexity))]
 #[cfg_attr(feature = "cargo-clippy", allow(collapsible_if))]
-pub fn test_machine(v: &Value, machine: &SeqContextVM<VMTestPatch>, block: &JSONBlock, history: &[Context], debug: bool) -> bool {
+pub fn test_machine<P: Patch>(v: &Value, machine: &SeqContextVM<P>, block: &JSONBlock, history: &[Context], debug: bool) -> bool {
     let callcreates = &v["callcreates"];
 
     if callcreates.as_array().is_some() {
@@ -262,13 +262,13 @@ fn is_ok(status: &VMStatus) -> bool {
     }
 }
 
-pub fn test_transaction(_name: &str, v: &Value, debug: bool) -> Result<bool, VMStatus> {
+pub fn test_transaction<P: Patch>(_name: &str, v: &Value, debug: bool) -> Result<bool, VMStatus> {
     let _ = env_logger::try_init();
 
     let mut block = create_block(v);
     let history: Arc<Mutex<Vec<Context>>> = Arc::new(Mutex::new(Vec::new()));
     let history_closure = history.clone();
-    let mut machine = create_machine(v, &block);
+    let mut machine = create_machine::<P>(v, &block);
     machine.add_context_history_hook(move |context| {
         history_closure.lock().unwrap().push(context.clone());
     });
@@ -299,13 +299,13 @@ pub fn test_transaction(_name: &str, v: &Value, debug: bool) -> Result<bool, VMS
 
 use criterion::Criterion;
 
-pub fn bench_transaction(_name: &str, v: Value, c: &mut Criterion) {
+pub fn bench_transaction<P: Patch>(_name: &str, v: Value, c: &mut Criterion) {
     c.bench_function(_name, move |b| {
         b.iter_with_large_setup(|| {
             let block = create_block(&v);
             let history: Arc<Mutex<Vec<Context>>> = Arc::new(Mutex::new(Vec::new()));
             let history_closure = history.clone();
-            let mut machine = create_machine(&v, &block);
+            let mut machine = create_machine::<P>(&v, &block);
             machine.add_context_history_hook(move |context| {
                 history_closure.lock().unwrap().push(context.clone());
             });

jsontests/src/util.rs
@@ -2,15 +2,16 @@ use serde_json::Value;
 use serde_json as json;
 use test_transaction;
 use bench_transaction;
+use evm::Patch;
 
-pub fn run_test(name: &str, test: &str) {
+pub fn run_test<P: Patch>(name: &str, test: &str) {
     let test: Value = json::from_str(test).unwrap();
-    assert_eq!(test_transaction(name, &test, true), Ok(true));
+    assert_eq!(test_transaction::<P>(name, &test, true), Ok(true));
 }
 
 use criterion::Criterion;
 
-pub fn run_bench(c: &mut Criterion, name: &'static str, test: &str) {
+pub fn run_bench<P: Patch>(c: &mut Criterion, name: &'static str, test: &str) {
     let test: Value = json::from_str(test).unwrap();
-    bench_transaction(name, test, c);
+    bench_transaction::<P>(name, test, c);
 }