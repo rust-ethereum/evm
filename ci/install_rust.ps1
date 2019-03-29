6f0a89e8c029c103bb4900ebd308ca471b7e14ab
Mike Lubinets <public@mersinvald.me>
2019-03-29 15:03:45 +0100

    fix(ci/windows): skip rustup install if Rust if already installed

--- ci/install_rust.ps1
+++ ci/install_rust.ps1
@@ -1,3 +1,7 @@
+if (Test-Path -Path C:\Users\jenkins\.cargo\bin\rustup.exe) {
+    exit
+}
+
 $client = new-object System.Net.WebClient
 $client.DownloadFile('https://win.rustup.rs', "$pwd\rustup-init.exe")
 .\rustup-init.exe -y
