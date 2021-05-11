19ade858c430ab13eb562764a870ac9f8506f8dd
Wei Tang <wei@that.world>
2021-05-11 17:33:30 +0200

    Skip setting memory value if the value vec is empty

--- core/src/memory.rs
+++ core/src/memory.rs
@@ -105,6 +105,10 @@ impl Memory {
 		value: &[u8],
 		target_size: Option<usize>
 	) -> Result<(), ExitFatal> {
+		if value.is_empty() {
+			return Ok(())
+		}
+		
 		let target_size = target_size.unwrap_or(value.len());
 
 		if offset.checked_add(target_size)
