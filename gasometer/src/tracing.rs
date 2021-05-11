cc5448498a27c9c7046a9ea118a6490736a890d2
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-05-11 14:20:50 +0000

    fix macro

--- gasometer/src/tracing.rs
+++ gasometer/src/tracing.rs
@@ -1,29 +1,55 @@
 //! Allows to listen to gasometer events.
 
 #[cfg(feature = "tracing")]
-environmental::environmental!(hook: dyn EventListener + 'static);
+pub(crate) mod inner {
+    environmental::environmental!(hook: dyn EventListener + 'static);
 
-#[cfg(feature = "tracing")]
-pub trait EventListener {
-    fn record_cost(cost: u64) { }
-    fn record_refund(refund: i64) { }
-    fn record_stipend(stipend: u64) { }
-    fn record_dynamic_cost(
-        gas_cost: u64,
-        memory_gas: u64,
-        gas_refund: i64
-    ) { }
-    fn record_transaction(transaction_cost: u64) { }
+    pub trait EventListener {
+        fn record_cost(
+            &mut self,
+            _cost: u64
+        ) { }
+
+        fn record_refund(
+            &mut self,
+            _refund: i64
+        ) { }
+
+        fn record_stipend(
+            &mut self,
+            _stipend: u64
+        ) { }
+
+        fn record_dynamic_cost(
+            &mut self, 
+            _gas_cost: u64,
+            _memory_gas: u64,
+            _gas_refund: i64
+        ) { }
+
+        fn record_transaction(
+            &mut self,
+            _transaction_cost: u64
+        ) { }
+    }
+
+    /// Run closure with provided listener.
+    pub fn using<R, F: FnOnce() -> R>(
+        listener: &mut (dyn EventListener + 'static),
+        f: F
+    ) -> R {
+        hook::using(listener, f)
+    }
+
+    pub(crate) fn with<F: FnOnce(&mut (dyn EventListener + 'static))>(
+        f: F
+    ) {
+        hook::with(f);
+    }
 }
 
-/// Run closure with provided listener.
 #[cfg(feature = "tracing")]
-pub fn using<R, F: FnOnce() -> R>(
-    listener: &mut (dyn EventListener + 'static),
-    f: F
-) -> R {
-    hook::using(listener, f)
-}
+pub use inner::using;
 
 #[cfg(not(feature = "tracing"))]
 macro_rules! event {
@@ -33,6 +59,6 @@ macro_rules! event {
 #[cfg(feature = "tracing")]
 macro_rules! event {
     ($method: ident ( $($arg:expr),* )) => {
-        hook::with(|hook| hook.$method( $($arg)* ));
+        $crate::tracing::inner::with(|hook| hook.$method( $($arg),* ));
     }
 }
