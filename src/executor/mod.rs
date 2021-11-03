43a595ac05fd133b0bd24b4b5923ab76ac12d2a2
nanocryk <6422796+nanocryk@users.noreply.github.com>
2021-11-03 14:15:01 +0000

    Prevent memory impl to access private fields + add missing pub/accessors (#80)
    * prevent memory impl to access private fields + add missing pub/accessors
    
    * move stack executor in its own submodule

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -2,13 +2,7 @@
 //!
 //! Executors are structs that hook gasometer and the EVM core together. It
 //! also handles the call stacks in EVM.
+//!
+//! Currently only a stack-based (customizable) executor is provided.
 
-mod stack;
-
-pub use self::stack::{
-	MemoryStackAccount, MemoryStackState, MemoryStackSubstate, PrecompileFailure, PrecompileFn,
-	PrecompileOutput, PrecompileSet, StackExecutor, StackExitKind, StackState,
-	StackSubstateMetadata,
-};
-
-pub use ethereum::Log;
+pub mod stack;
