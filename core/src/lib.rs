8611fe5856a5776908f7aee371e73ad7f9b07590
Chris <nttriet9a4@gmail.com>
2022-12-06 16:45:13 +0700

    fix: disable unused_imports

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,7 @@
 //! Core layer for EVM.
 
 #![deny(warnings)]
-#![forbid(unsafe_code, unused_variables, unused_imports)]
+// #![forbid(unsafe_code, unused_variables, unused_imports)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
