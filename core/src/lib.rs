8517d12bbadbcc80c1afb00b0b298e000668a974
bzawisto <bartosz@zilliqa.com>
2022-12-15 12:41:33 +0100

    Extending machine interface (2)

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -66,6 +66,14 @@ impl Machine {
 	pub fn position(&self) -> &Result<usize, ExitReason> {
 		&self.position
 	}
+	/// Return a reference of the return range.
+	pub fn return_range(&self) -> &Range<U256> {
+		&self.return_range
+	}
+	/// Return a reference of the valids.
+	pub fn return_valids(&self) -> &Valids {
+		&self.valids
+	}
 
 	/// Create a new machine with given code and data.
 	pub fn new(
