1e318692de81bec946662dac4ccf438220787c51
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2024-04-19 09:35:59 +0100

    burn base fee after eip-1559 included in london fork

--- src/standard/config.rs
+++ src/standard/config.rs
@@ -97,6 +97,8 @@ pub struct Config {
 	pub has_base_fee: bool,
 	/// Has PUSH0 opcode. See [EIP-3855](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-3855.md)
 	pub has_push0: bool,
+	/// Uses EIP-1559 (Base fee is burned when this flag is enabled) [EIP-1559](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1559.md)
+	pub eip_1559: bool,
 }
 
 impl Config {
@@ -150,6 +152,7 @@ impl Config {
 			has_ext_code_hash: false,
 			has_base_fee: false,
 			has_push0: false,
+			eip_1559: false,
 		}
 	}
 
@@ -203,6 +206,7 @@ impl Config {
 			has_ext_code_hash: true,
 			has_base_fee: false,
 			has_push0: false,
+			eip_1559: false,
 		}
 	}
 
@@ -237,6 +241,7 @@ impl Config {
 			disallow_executable_format,
 			warm_coinbase_address,
 			max_initcode_size,
+			eip_1559,
 		} = inputs;
 
 		// See https://eips.ethereum.org/EIPS/eip-2929
@@ -299,6 +304,7 @@ impl Config {
 			has_ext_code_hash: true,
 			has_base_fee,
 			has_push0,
+			eip_1559,
 		}
 	}
 }
@@ -315,6 +321,7 @@ struct DerivedConfigInputs {
 	disallow_executable_format: bool,
 	warm_coinbase_address: bool,
 	max_initcode_size: Option<usize>,
+	eip_1559: bool,
 }
 
 impl DerivedConfigInputs {
@@ -329,6 +336,7 @@ impl DerivedConfigInputs {
 			disallow_executable_format: false,
 			warm_coinbase_address: false,
 			max_initcode_size: None,
+			eip_1559: false,
 		}
 	}
 
@@ -343,6 +351,7 @@ impl DerivedConfigInputs {
 			disallow_executable_format: true,
 			warm_coinbase_address: false,
 			max_initcode_size: None,
+			eip_1559: true,
 		}
 	}
 
@@ -357,6 +366,7 @@ impl DerivedConfigInputs {
 			disallow_executable_format: true,
 			warm_coinbase_address: false,
 			max_initcode_size: None,
+			eip_1559: true,
 		}
 	}
 
@@ -372,6 +382,7 @@ impl DerivedConfigInputs {
 			warm_coinbase_address: true,
 			// 2 * 24576 as per EIP-3860
 			max_initcode_size: Some(0xC000),
+			eip_1559: true,
 		}
 	}
 }
