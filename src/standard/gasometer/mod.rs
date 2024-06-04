a5a79c4de4efc3fde78aa2e590ac33de29e5467b
Rodrigo Quelhas <22591718+RomarQ@users.noreply.github.com>
2024-06-04 12:42:50 +0100

    Fix eip2929 implementation (#280)
    * burn base fee after eip-1559 included in london fork
    
    * feat: add Cancun configuration
    
    * fix(eip-2929): some addresses were considered cold when they shouldn't
    
    * Revert "feat: add Cancun configuration"
    
    This reverts commit 654fb20800753f164af96b0f2fd41e62fc8794b2.
    
    * Revert "burn base fee after eip-1559 included in london fork"
    
    This reverts commit 1e318692de81bec946662dac4ccf438220787c51.
    
    * add eip-2929 test
    
    * remove duplicated test

--- src/standard/gasometer/mod.rs
+++ src/standard/gasometer/mod.rs
@@ -283,7 +283,7 @@ fn dynamic_opcode_cost<H: RuntimeBackend>(
 	stack: &Stack,
 	is_static: bool,
 	config: &Config,
-	handler: &H,
+	handler: &mut H,
 ) -> Result<(GasCost, Option<MemoryCost>), ExitError> {
 	let gas_cost = match opcode {
 		Opcode::RETURN => GasCost::Zero,
@@ -307,40 +307,59 @@ fn dynamic_opcode_cost<H: RuntimeBackend>(
 
 		Opcode::EXTCODESIZE => {
 			let target = stack.peek(0)?.into();
-			GasCost::ExtCodeSize {
-				target_is_cold: handler.is_cold(target, None),
-			}
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
+			GasCost::ExtCodeSize { target_is_cold }
 		}
 		Opcode::BALANCE => {
 			let target = stack.peek(0)?.into();
-			GasCost::Balance {
-				target_is_cold: handler.is_cold(target, None),
-			}
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
+			GasCost::Balance { target_is_cold }
 		}
 		Opcode::BLOCKHASH => GasCost::BlockHash,
 
 		Opcode::EXTCODEHASH if config.has_ext_code_hash => {
 			let target = stack.peek(0)?.into();
-			GasCost::ExtCodeHash {
-				target_is_cold: handler.is_cold(target, None),
-			}
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
+			GasCost::ExtCodeHash { target_is_cold }
 		}
 		Opcode::EXTCODEHASH => GasCost::Invalid(opcode),
 
 		Opcode::CALLCODE => {
 			let target = stack.peek(1)?.into();
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
 			GasCost::CallCode {
 				value: U256::from_big_endian(&stack.peek(2)?[..]),
 				gas: U256::from_big_endian(&stack.peek(0)?[..]),
-				target_is_cold: handler.is_cold(target, None),
+				target_is_cold,
 				target_exists: { handler.exists(target) },
 			}
 		}
 		Opcode::STATICCALL => {
 			let target = stack.peek(1)?.into();
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
 			GasCost::StaticCall {
 				gas: U256::from_big_endian(&stack.peek(0)?[..]),
-				target_is_cold: handler.is_cold(target, None),
+				target_is_cold,
 				target_exists: { handler.exists(target) },
 			}
 		}
@@ -349,8 +368,13 @@ fn dynamic_opcode_cost<H: RuntimeBackend>(
 		},
 		Opcode::EXTCODECOPY => {
 			let target = stack.peek(0)?.into();
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
 			GasCost::ExtCodeCopy {
-				target_is_cold: handler.is_cold(target, None),
+				target_is_cold,
 				len: U256::from_big_endian(&stack.peek(3)?[..]),
 			}
 		}
@@ -365,17 +389,25 @@ fn dynamic_opcode_cost<H: RuntimeBackend>(
 		},
 		Opcode::SLOAD => {
 			let index = stack.peek(0)?;
-			GasCost::SLoad {
-				target_is_cold: handler.is_cold(address, Some(index)),
-			}
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(address, Some(index));
+			handler.mark_hot(address, Some(index));
+
+			GasCost::SLoad { target_is_cold }
 		}
 		Opcode::TLOAD if config.eip_1153_enabled => GasCost::TLoad,
 
 		Opcode::DELEGATECALL if config.has_delegate_call => {
 			let target = stack.peek(1)?.into();
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
 			GasCost::DelegateCall {
 				gas: U256::from_big_endian(&stack.peek(0)?[..]),
-				target_is_cold: handler.is_cold(target, None),
+				target_is_cold,
 				target_exists: { handler.exists(target) },
 			}
 		}
@@ -390,11 +422,16 @@ fn dynamic_opcode_cost<H: RuntimeBackend>(
 		Opcode::SSTORE if !is_static => {
 			let index = stack.peek(0)?;
 			let value = stack.peek(1)?;
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(address, Some(index));
+			handler.mark_hot(address, Some(index));
+
 			GasCost::SStore {
 				original: handler.original_storage(address, index),
 				current: handler.storage(address, index),
 				new: value,
-				target_is_cold: handler.is_cold(address, Some(index)),
+				target_is_cold,
 			}
 		}
 		Opcode::TSTORE if !is_static && config.eip_1153_enabled => GasCost::TStore,
@@ -424,9 +461,14 @@ fn dynamic_opcode_cost<H: RuntimeBackend>(
 		},
 		Opcode::SUICIDE if !is_static => {
 			let target = stack.peek(0)?.into();
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
 			GasCost::Suicide {
 				value: handler.balance(address),
-				target_is_cold: handler.is_cold(target, None),
+				target_is_cold,
 				target_exists: { handler.exists(target) },
 				already_removed: handler.deleted(address),
 			}
@@ -436,10 +478,15 @@ fn dynamic_opcode_cost<H: RuntimeBackend>(
 				|| (is_static && U256::from_big_endian(&stack.peek(2)?[..]) == U256::zero()) =>
 		{
 			let target = stack.peek(1)?.into();
+
+			// https://eips.ethereum.org/EIPS/eip-2929
+			let target_is_cold = handler.is_cold(target, None);
+			handler.mark_hot(target, None);
+
 			GasCost::Call {
 				value: U256::from_big_endian(&stack.peek(2)?[..]),
 				gas: U256::from_big_endian(&stack.peek(0)?[..]),
-				target_is_cold: handler.is_cold(target, None),
+				target_is_cold,
 				target_exists: { handler.exists(target) },
 			}
 		}
