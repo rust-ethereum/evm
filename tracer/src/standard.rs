d96734224e67705369d9f1b3e89ba78c7da5bc0d
Voronor <129545215+voronor@users.noreply.github.com>
2024-11-22 14:30:05 +0100

    Fix: Avoid Infinite Recursion in on_eval Method

--- tracer/src/standard.rs
+++ tracer/src/standard.rs
@@ -9,6 +9,6 @@ pub trait EvalTracer<H> {
 
 impl<'config, H, T: EvalTracer<H>> crate::EvalTracer<State<'config>, H> for T {
 	fn on_eval(&mut self, machine: &Machine, handle: &H, opcode: Opcode, position: usize) {
-		EvalTracer::<H>::on_eval(self, machine, handle, opcode, position)
+		self.on_eval(machine, handle, opcode, position)
 	}
 }
