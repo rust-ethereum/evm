fd16a8369917f17bdd27bac1d75ee9af90b31a36
Wei Tang <wei@pacna.org>
2025-05-25 14:02:08 +0200

    Implement evm64 opcodes and gasometer

--- features/evm64/src/gasometer.rs
+++ features/evm64/src/gasometer.rs
@@ -1,15 +1,86 @@
 use evm::{
-	interpreter::{etable::Control, machine::Machine},
+	interpreter::{error::ExitException, etable::Control, machine::Machine, opcode::Opcode},
 	standard::GasometerState,
 };
 
+macro_rules! try_or_fail {
+	($e:expr) => {
+		match $e {
+			Ok(v) => v,
+			Err(e) => return Control::Exit(e.into()),
+		}
+	};
+}
+
 pub fn eval<'config, S, H, Tr>(
-	_machine: &mut Machine<S>,
+	machine: &mut Machine<S>,
 	_handler: &mut H,
-	_position: usize,
+	position: usize,
 ) -> Control<Tr>
 where
 	S: AsRef<GasometerState<'config>> + AsMut<GasometerState<'config>>,
 {
-	todo!()
+	const _G_BASE64: u64 = 1;
+	const G_VERYLOW64: u64 = 2;
+	const G_LOW64: u64 = 3;
+	const G_MID64: u64 = 5;
+	const G_HIGH64: u64 = 7;
+	const G_EXP64_STATIC: u64 = 5;
+	const G_EXP64_DYNAMIC: u64 = 25;
+
+	const STATIC_COST_TABLE: [u64; 256] = {
+		let mut table = [0; 256];
+		table[Opcode::ADD.as_usize()] = G_VERYLOW64;
+		table[Opcode::SUB.as_usize()] = G_VERYLOW64;
+		table[Opcode::MUL.as_usize()] = G_LOW64;
+		table[Opcode::DIV.as_usize()] = G_LOW64;
+		table[Opcode::SDIV.as_usize()] = G_LOW64;
+		table[Opcode::MOD.as_usize()] = G_LOW64;
+		table[Opcode::SMOD.as_usize()] = G_LOW64;
+		table[Opcode::ADDMOD.as_usize()] = G_MID64;
+		table[Opcode::MULMOD.as_usize()] = G_MID64;
+		table[Opcode::LT.as_usize()] = G_VERYLOW64;
+		table[Opcode::GT.as_usize()] = G_VERYLOW64;
+		table[Opcode::SLT.as_usize()] = G_VERYLOW64;
+		table[Opcode::SGT.as_usize()] = G_VERYLOW64;
+		table[Opcode::EQ.as_usize()] = G_VERYLOW64;
+		table[Opcode::AND.as_usize()] = G_VERYLOW64;
+		table[Opcode::OR.as_usize()] = G_VERYLOW64;
+		table[Opcode::XOR.as_usize()] = G_VERYLOW64;
+		table[Opcode::ISZERO.as_usize()] = G_VERYLOW64;
+		table[Opcode::NOT.as_usize()] = G_VERYLOW64;
+		table[Opcode::SHL.as_usize()] = G_VERYLOW64;
+		table[Opcode::SHR.as_usize()] = G_VERYLOW64;
+		table[Opcode::SAR.as_usize()] = G_VERYLOW64;
+		table[Opcode::JUMP.as_usize()] = G_MID64;
+		table[Opcode::JUMPI.as_usize()] = G_HIGH64;
+
+		table
+	};
+
+	let opcode = Opcode(machine.code()[position]);
+
+	try_or_fail!(machine
+		.state
+		.as_mut()
+		.record_gas64(STATIC_COST_TABLE[opcode.as_usize()]));
+
+	#[allow(clippy::single_match)]
+	match opcode {
+		Opcode::EXP => {
+			let power = try_or_fail!(machine.stack.peek(1)).0[0];
+			let cost = if power == 0 {
+				G_EXP64_STATIC
+			} else {
+				try_or_fail!(G_EXP64_DYNAMIC
+					.checked_mul(power.ilog2() as u64 / 8 + 1)
+					.and_then(|dynamic| G_EXP64_STATIC.checked_add(dynamic))
+					.ok_or(ExitException::OutOfGas))
+			};
+			try_or_fail!(machine.state.as_mut().record_gas64(cost));
+		}
+		_ => (),
+	}
+
+	Control::NoAction
 }
--- features/evm64/src/lib.rs
+++ features/evm64/src/lib.rs
@@ -30,6 +30,30 @@ where
 
 	let mut mode_etable = Etable::none();
 	mode_etable[Opcode::ADD.as_usize()] = eval::eval_add;
+	mode_etable[Opcode::MUL.as_usize()] = eval::eval_mul;
+	mode_etable[Opcode::SUB.as_usize()] = eval::eval_sub;
+	mode_etable[Opcode::DIV.as_usize()] = eval::eval_div;
+	mode_etable[Opcode::SDIV.as_usize()] = eval::eval_sdiv;
+	mode_etable[Opcode::MOD.as_usize()] = eval::eval_mod;
+	mode_etable[Opcode::SMOD.as_usize()] = eval::eval_smod;
+	mode_etable[Opcode::ADDMOD.as_usize()] = eval::eval_addmod;
+	mode_etable[Opcode::MULMOD.as_usize()] = eval::eval_mulmod;
+	mode_etable[Opcode::EXP.as_usize()] = eval::eval_exp;
+	mode_etable[Opcode::LT.as_usize()] = eval::eval_lt;
+	mode_etable[Opcode::GT.as_usize()] = eval::eval_gt;
+	mode_etable[Opcode::SLT.as_usize()] = eval::eval_slt;
+	mode_etable[Opcode::SGT.as_usize()] = eval::eval_sgt;
+	mode_etable[Opcode::EQ.as_usize()] = eval::eval_eq;
+	mode_etable[Opcode::ISZERO.as_usize()] = eval::eval_iszero;
+	mode_etable[Opcode::AND.as_usize()] = eval::eval_and;
+	mode_etable[Opcode::OR.as_usize()] = eval::eval_or;
+	mode_etable[Opcode::XOR.as_usize()] = eval::eval_xor;
+	mode_etable[Opcode::NOT.as_usize()] = eval::eval_not;
+	mode_etable[Opcode::SHL.as_usize()] = eval::eval_shl;
+	mode_etable[Opcode::SHR.as_usize()] = eval::eval_shr;
+	mode_etable[Opcode::SAR.as_usize()] = eval::eval_sar;
+	mode_etable[Opcode::JUMP.as_usize()] = eval::eval_jump;
+	mode_etable[Opcode::JUMPI.as_usize()] = eval::eval_jumpi;
 
 	gasometer_etable[OPCODE_EVM64_MODE.as_usize()] = eval_gasometer;
 	eval_etable[OPCODE_EVM64_MODE.as_usize()] = MultiEfn::Node(Box::new(mode_etable.into()));
