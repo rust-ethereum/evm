47a2f2290a57a98b789c78374c399aaafb18a230
Rodrigo Quelhas <rodrigo_quelhas@outlook.pt>
2025-07-07 09:46:04 +0100

    fix build

--- src/executor/stack/executor.rs
+++ src/executor/stack/executor.rs
@@ -1413,8 +1413,8 @@ impl<'config, S: StackState<'config>, P: PrecompileSet> Handler
 	fn log(&mut self, address: H160, topics: Vec<H256>, data: Vec<u8>) -> Result<(), ExitError> {
 		event!(Log {
 			address,
-			topics,
-			data
+			topics: &topics,
+			data: &data
 		});
 		self.state.log(address, topics, data);
 		Ok(())
