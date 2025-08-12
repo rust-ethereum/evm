a5dbaac90584981d9e7a33ffec386db0e63e6162
Wei Tang <wei@pacna.org>
2025-08-12 06:57:01 +0200

    Delete evm-tracer
    The crate is no longer needed. Tracing can be easily implemented as an
    additional Etable component.

--- tracer/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod standard;
-
-use evm::interpreter::{Machine, Opcode};
-
-pub trait EvalTracer<S, H> {
-	fn on_eval(&mut self, machine: &Machine<S>, handle: &H, opcode: Opcode, position: usize);
-}
