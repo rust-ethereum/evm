cd749a183af2c5dbcaaa72d471c6f69ab5902755
Wei Tang <wei@pacna.org>
2025-08-13 22:59:52 +0200

    Remove hyphen

--- jsontests/tests/old_ethtests.rs
+++
@@ -1,3 +0,0 @@
-use jsontests_proc_macro::statetest_folder;
-
-statetest_folder!("old_ethtests", "../res/ethtests/GeneralStateTests");
---
+++ jsontests/tests/oldethtests.rs
@@ -0,0 +1,3 @@
+use jsontests_proc_macro::statetest_folder;
+
+statetest_folder!("oldethtests", "../res/ethtests/GeneralStateTests");
